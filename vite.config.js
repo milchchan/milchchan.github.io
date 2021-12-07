13a4d96d10ad2a9eafe78f07b8dd0b8bd161e96f
Milch <kawatan@gmail.com>
2021-12-07 14:12:50 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    minify: false,
-    outDir: "docs",
+    outDir: "dist",
     rollupOptions: {
       output: {
         entryFileNames: "[name].js",
