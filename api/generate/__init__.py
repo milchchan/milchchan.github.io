65bfb98ed177ed95eec573d74e417f2fee9597a3
Milch <kawatan@gmail.com>
2022-01-05 00:50:53 +0900

    Add generate function

---
+++ api/generate/__init__.py
@@ -0,0 +1,72 @@
+import logging
+import time
+import json
+import os
+from datetime import timedelta
+
+import azure.functions as func
+
+from google.oauth2 import service_account
+from google.cloud import storage
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    headers = {'Content-Type': 'application/json'}
+
+    if 'Origin' in req.headers:
+        headers['Access-Control-Allow-Origin'] = req.headers['Origin']
+
+    try:
+        if req.headers['Content-Type'] == 'application/json':
+            data = req.get_json()
+            path = data.get('path')
+            expiration = data.get('expiration')
+            expiration = timedelta(seconds=expiration if expiration else 3600)
+            
+        else:
+            path = req.params.get('path')
+            expiration = req.params.get('expiration')
+            expiration = timedelta(seconds=int(expiration) if expiration and expiration.isnumeric() else 3600)
+
+        if path is not None:                
+            credentials = service_account.Credentials.from_service_account_info({
+                'type': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TYPE'),
+                'project_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID'),
+                'private_key_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID'),
+                'private_key': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY'),
+                'client_email': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL'),
+                'client_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID'),
+                'auth_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_URL'),
+                'token_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URL'),
+                'auth_provider_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL'),
+                'client_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_X509_CERT_URL')
+            })
+            scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
+            storage_client = storage.Client(credentials=scoped_credentials, project=scoped_credentials.project_id)
+            bucket = storage_client.bucket('merkuchan.com')
+            blob = bucket.blob(path)
+
+            if blob.exists():
+                return func.HttpResponse(json.dumps({
+                        'url': blob.generate_signed_url(
+                            version='v4',
+                            expiration=expiration,
+                            method='GET'),
+                        'timestamp': int(time.time())
+                    }),
+                    status_code=200,
+                    headers=headers,
+                    charset='utf-8')
+
+        return func.HttpResponse(status_code=400, headers=headers)
+
+    except Exception as e:
+        logging.error(f'{e}')
+
+        return func.HttpResponse(json.dumps({
+                'error': {
+                    'message': str(e),
+                    'type': type(e).__name__ }
+            }),
+            status_code=400,
+            headers=headers,
+            charset='utf-8')
