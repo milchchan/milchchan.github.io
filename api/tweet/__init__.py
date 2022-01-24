9518e9063e4b08f8e13f0e8c5ddee928ccc29033
Milch <kawatan@gmail.com>
2022-01-24 17:55:32 +0900

    Add tweet function

---
+++ api/tweet/__init__.py
@@ -0,0 +1,69 @@
+import json
+import logging
+import os
+from urllib.request import urlopen, Request
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    headers = {'Content-Type': 'application/json'}
+
+    if 'Origin' in req.headers:
+        headers['Access-Control-Allow-Origin'] = req.headers['Origin']
+
+    try:
+        if req.method == 'POST':
+            if 'Authorization' in req.headers:
+                jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith('Bearer ') else req.headers['Authorization'].split('.')
+
+                if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://securetoken.google.com/milchchan':
+                    try:
+                        response = urlopen(Request(
+                            f'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key={os.environ.get("FIREBASE_API_KEY")}',
+                            headers={'Content-Type': 'application/json'},
+                            data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
+
+                        if response.getcode() != 200:
+                            raise Exception
+
+                    except Exception:
+                        return func.HttpResponse(status_code=403, headers=headers)
+                        
+            if req.headers.get('Content-Type') == 'application/json':
+                data = req.get_json()
+                email = data.get('email')
+                password = data.get('password')
+                response = urlopen(Request(
+                        f'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={os.environ.get("FIREBASE_API_KEY")}',
+                        headers={'Content-Type': 'application/json'},
+                        data=json.dumps({'email': email, 'password': password, 'returnSecureToken': True}).encode('utf-8'),
+                        method='POST'))
+
+                if response.getcode() == 200:
+                    results = json.loads(response.read())
+
+                    return func.HttpResponse(json.dumps({'id': results['localId'], 'token': results['idToken'], 'timestamp': int(time.time())}), status_code=200, headers=headers, charset='utf-8')
+
+            else:
+                return func.HttpResponse(status_code=400, headers=headers)
+            
+        elif req.method == 'GET':
+
+            pass
+
+        headers['Allow'] = 'GET, POST'
+
+        return func.HttpResponse(status_code=405, headers=headers)
+
+    except Exception as e:
+        logging.error(f'{e}')
+
+        return func.HttpResponse(json.dumps({
+                'error': {
+                    'message': str(e),
+                    'type': type(e).__name__ }
+            }),
+            status_code=400,
+            headers=headers,
+            charset='utf-8')
