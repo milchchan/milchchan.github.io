2576383c0c312387a0ec5c0074bb862a32269935
Milch <kawatan@gmail.com>
2022-01-25 19:08:54 +0900

    Update __init__.py

--- api/recent/__init__.py
+++ api/recent/__init__.py
@@ -15,111 +15,112 @@ from google.cloud.storage.blob import Blob
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    headers = {'Content-Type': 'application/json'}
-
-    if 'Origin' in req.headers:
-        headers['Access-Control-Allow-Origin'] = req.headers['Origin']
-
     try:
-        if req.method == 'GET':
-            if 'Authorization' in req.headers:
-                jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith('Bearer ') else req.headers['Authorization'].split('.')
+        if 'Authorization' in req.headers:
+            jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith(
+                'Bearer ') else req.headers['Authorization'].split('.')
 
-                if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://securetoken.google.com/milchchan':
-                    try:
-                        response = urlopen(Request(
-                            f'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key={os.environ.get("FIREBASE_API_KEY")}',
-                            headers={'Content-Type': 'application/json'},
-                            data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
+            if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://securetoken.google.com/milchchan':
+                try:
+                    response = urlopen(Request(
+                        f'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key={os.environ.get("FIREBASE_API_KEY")}',
+                        headers={'Content-Type': 'application/json'},
+                        data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
 
-                        if response.getcode() != 200:
-                            raise Exception
+                    if response.getcode() != 200:
+                        raise Exception
 
-                    except Exception:
-                        return func.HttpResponse(status_code=403, headers=headers)
-            
-            if req.headers.get('Content-Type') == 'application/json':
-                data = req.get_json()
-                offset = data.get('offset')
-                limit = data.get('limit')
-                geohash = data.get('geohash')
-                
-            else:
-                offset = int(req.params['offset']) if 'offset' in req.params else None
-                limit = int(req.params['limit']) if 'limit' in req.params else None
-                geohash = req.params['geohash'] if 'geohash' in req.params else None
+                except Exception:
+                    return func.HttpResponse(status_code=403, mimetype='', charset='')
 
-            client = CosmosClient.from_connection_string(os.environ.get('AZURE_COSMOS_DB_CONNECTION_STRING'))
-            database = client.get_database_client('Wonderland')
-            container = database.get_container_client('Words')
-            
-            if geohash is None:
-                items = list(container.query_items(
-                    query='SELECT w.id, w.name, w.attributes, w.image, w.location, w.timestamp FROM Words w ORDER BY w.timestamp DESC OFFSET @offset LIMIT @limit',
-                    parameters=[
-                        { "name":"@offset", "value": 0 if offset is None else offset },
-                        { "name":"@limit", "value": 100 if limit is None else limit }
-                    ],
-                    enable_cross_partition_query=True))
-                    
-            else:
-                neighbors = get_neighbors(geohash)
-                items = list(container.query_items(
-                    query="SELECT w.id, w.name, w.attributes, w.image, w.location, w.timestamp FROM Words w WHERE w.geohash LIKE CONCAT(@centergeohash, '%') OR w.geohash LIKE CONCAT(@topgeohash, '%') OR w.geohash LIKE CONCAT(@bottomgeohash, '%') OR w.geohash LIKE CONCAT(@rightgeohash, '%') OR w.geohash LIKE CONCAT(@leftgeohash, '%') OR w.geohash LIKE CONCAT(@topleftgeohash, '%') OR w.geohash LIKE CONCAT(@toprightgeohash, '%') OR w.geohash LIKE CONCAT(@bottomrightgeohash, '%') OR w.geohash LIKE CONCAT(@bottomleftgeohash, '%') ORDER BY w.timestamp DESC OFFSET @offset LIMIT @limit",
-                    parameters=[
-                        { "name":"@offset", "value": 0 if offset is None else offset },
-                        { "name":"@limit", "value": 100 if limit is None else limit },
-                        { "name":"@centergeohash", "value": geohash },
-                        { "name":"@topgeohash", "value": neighbors['top'] },
-                        { "name":"@bottomgeohash", "value": neighbors['bottom'] },
-                        { "name":"@rightgeohash", "value": neighbors['right'] },
-                        { "name":"@leftgeohash", "value": neighbors['left'] },
-                        { "name":"@topleftgeohash", "value": neighbors['topleft'] },
-                        { "name":"@toprightgeohash", "value": neighbors['topright'] },
-                        { "name":"@bottomrightgeohash", "value": neighbors['bottomright'] },
-                        { "name":"@bottomleftgeohash", "value": neighbors['bottomleft'] }
-                    ],
-                    enable_cross_partition_query=True))
+        if req.headers.get('Content-Type') == 'application/json':
+            data = req.get_json()
+            offset = data.get('offset')
+            limit = data.get('limit')
+            geohash = data.get('geohash')
 
-            for item in items:
-                if 'image' in item:
-                    credentials = service_account.Credentials.from_service_account_info({
-                        'type': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TYPE'),
-                        'project_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID'),
-                        'private_key_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID'),
-                        'private_key': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY').replace('\\n', '\n'),
-                        'client_email': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL'),
-                        'client_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID'),
-                        'auth_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_URI'),
-                        'token_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URI'),
-                        'auth_provider_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL'),
-                        'client_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_X509_CERT_URL')
-                    })
-                    scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
-                    blob = Blob.from_string(item['image'], client=storage.Client(credentials=scoped_credentials, project=scoped_credentials.project_id))
-                
-                    if blob.exists():
-                        item['image'] = blob.generate_signed_url(version='v4', expiration=timedelta(minutes=60), method='GET')
+        else:
+            offset = int(req.params['offset']
+                         ) if 'offset' in req.params else None
+            limit = int(req.params['limit']) if 'limit' in req.params else None
+            geohash = req.params['geohash'] if 'geohash' in req.params else None
 
-                if 'location' in item and item['location'] is not None and 'type' in item['location'] and item['location']['type'] == 'Point' and 'coordinates' in item['location']:
-                    item['location'] = {'type': 'Point', 'coordinates': item['location']['coordinates']}
+        client = CosmosClient.from_connection_string(
+            os.environ.get('AZURE_COSMOS_DB_CONNECTION_STRING'))
+        database = client.get_database_client('Wonderland')
+        container = database.get_container_client('Words')
 
-                item['timestamp'] = int(datetime.utcfromtimestamp(datetime.fromisoformat(item['timestamp'].replace('Z', '+00:00')).timestamp()).timestamp())
+        if geohash is None:
+            items = list(container.query_items(
+                query='SELECT w.id, w.name, w.attributes, w.image, w.location, w.timestamp FROM Words w ORDER BY w.timestamp DESC OFFSET @offset LIMIT @limit',
+                parameters=[
+                    {"name": "@offset", "value": 0 if offset is None else offset},
+                    {"name": "@limit", "value": 100 if limit is None else limit}
+                ],
+                enable_cross_partition_query=True))
 
-            return func.HttpResponse(json.dumps(items), status_code=200, headers=headers, charset='utf-8')
+        else:
+            neighbors = get_neighbors(geohash)
+            items = list(container.query_items(
+                query="SELECT w.id, w.name, w.attributes, w.image, w.location, w.timestamp FROM Words w WHERE w.geohash LIKE CONCAT(@centergeohash, '%') OR w.geohash LIKE CONCAT(@topgeohash, '%') OR w.geohash LIKE CONCAT(@bottomgeohash, '%') OR w.geohash LIKE CONCAT(@rightgeohash, '%') OR w.geohash LIKE CONCAT(@leftgeohash, '%') OR w.geohash LIKE CONCAT(@topleftgeohash, '%') OR w.geohash LIKE CONCAT(@toprightgeohash, '%') OR w.geohash LIKE CONCAT(@bottomrightgeohash, '%') OR w.geohash LIKE CONCAT(@bottomleftgeohash, '%') ORDER BY w.timestamp DESC OFFSET @offset LIMIT @limit",
+                parameters=[
+                    {"name": "@offset", "value": 0 if offset is None else offset},
+                    {"name": "@limit", "value": 100 if limit is None else limit},
+                    {"name": "@centergeohash", "value": geohash},
+                    {"name": "@topgeohash", "value": neighbors['top']},
+                    {"name": "@bottomgeohash", "value": neighbors['bottom']},
+                    {"name": "@rightgeohash", "value": neighbors['right']},
+                    {"name": "@leftgeohash", "value": neighbors['left']},
+                    {"name": "@topleftgeohash", "value": neighbors['topleft']},
+                    {"name": "@toprightgeohash",
+                        "value": neighbors['topright']},
+                    {"name": "@bottomrightgeohash",
+                        "value": neighbors['bottomright']},
+                    {"name": "@bottomleftgeohash",
+                        "value": neighbors['bottomleft']}
+                ],
+                enable_cross_partition_query=True))
 
-        headers['Allow'] = 'GET'
+        for item in items:
+            if 'image' in item:
+                credentials = service_account.Credentials.from_service_account_info({
+                    'type': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TYPE'),
+                    'project_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID'),
+                    'private_key_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID'),
+                    'private_key': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY').replace('\\n', '\n'),
+                    'client_email': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL'),
+                    'client_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID'),
+                    'auth_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_URI'),
+                    'token_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URI'),
+                    'auth_provider_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL'),
+                    'client_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_X509_CERT_URL')
+                })
+                scoped_credentials = credentials.with_scopes(
+                    ['https://www.googleapis.com/auth/cloud-platform'])
+                blob = Blob.from_string(item['image'], client=storage.Client(
+                    credentials=scoped_credentials, project=scoped_credentials.project_id))
 
-        return func.HttpResponse(status_code=405, headers=headers)
+                if blob.exists():
+                    item['image'] = blob.generate_signed_url(
+                        version='v4', expiration=timedelta(minutes=60), method='GET')
+
+            if 'location' in item and item['location'] is not None and 'type' in item['location'] and item['location']['type'] == 'Point' and 'coordinates' in item['location']:
+                item['location'] = {
+                    'type': 'Point', 'coordinates': item['location']['coordinates']}
+
+            item['timestamp'] = int(datetime.utcfromtimestamp(datetime.fromisoformat(
+                item['timestamp'].replace('Z', '+00:00')).timestamp()).timestamp())
+
+        return func.HttpResponse(json.dumps(items), status_code=200, mimetype='application/json', charset='utf-8')
 
     except Exception as e:
         logging.error(f'{e}')
 
         return func.HttpResponse(json.dumps({
-                'error': {
-                    'message': str(e),
-                    'type': type(e).__name__ }
-            }),
+            'error': {
+                'message': str(e),
+                'type': type(e).__name__}
+        }),
             status_code=400,
-            headers=headers,
+            mimetype='application/json',
             charset='utf-8')
