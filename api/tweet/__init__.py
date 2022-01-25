46c123fd56c2c7d6a504e0e758e3e2cce7311476
Milch <kawatan@gmail.com>
2022-01-25 13:32:19 +0900

    Update __init__.py

--- api/tweet/__init__.py
+++ api/tweet/__init__.py
@@ -15,20 +15,11 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    headers = {}
-
-    if 'Origin' in req.headers:
-        headers['Access-Control-Allow-Origin'] = req.headers['Origin']
-    else:
-        headers['Access-Control-Allow-Origin'] = '*'
-
     try:
         if req.method == 'POST':
-            #headers = {'Content-Type': 'application/json'}
-            headers['Content-Type'] = 'application/json'
-
             if 'Authorization' in req.headers:
-                jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith('Bearer ') else req.headers['Authorization'].split('.')
+                jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith(
+                    'Bearer ') else req.headers['Authorization'].split('.')
 
                 if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://securetoken.google.com/milchchan':
                     try:
@@ -41,22 +32,26 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                             raise Exception
 
                     except Exception:
-                        return func.HttpResponse(status_code=403, headers=headers)
-                        
+                        return func.HttpResponse(status_code=403, mimetype='', charset='')
+
             if req.headers.get('Content-Type') == 'application/json':
                 data = req.get_json()
 
                 if 'status' in data:
-                    access_token = data['access_token'] if 'access_token' in data else os.environ.get("TWITTER_OAUTH_TOKEN")
-                    secret = data['secret'] if 'secret' in data else os.environ.get("TWITTER_OAUTH_TOKEN_SECRET")
+                    access_token = data['access_token'] if 'access_token' in data else os.environ.get(
+                        "TWITTER_OAUTH_TOKEN")
+                    secret = data['secret'] if 'secret' in data else os.environ.get(
+                        "TWITTER_OAUTH_TOKEN_SECRET")
                     status = data['status']
                     image = data.get('image')
 
                     CONSUMER_KEY = os.environ.get("TWITTER_CONSUMER_KEY")
                     CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET")
 
-                    timestamp = int(datetime.utcfromtimestamp(datetime.now(timezone.utc).timestamp()).timestamp())
-                    nonce = ''.join([str(random.randint(0, 9)) for i in range(8)])
+                    timestamp = int(datetime.utcfromtimestamp(
+                        datetime.now(timezone.utc).timestamp()).timestamp())
+                    nonce = ''.join([str(random.randint(0, 9))
+                                    for i in range(8)])
 
                     if image is None:
                         parameters = f"oauth_consumer_key={CONSUMER_KEY}&oauth_nonce={str(nonce)}&oauth_signature_method=HMAC-SHA1&oauth_timestamp={str(timestamp)}&oauth_token={access_token}&oauth_version=1.0&status={quote(status, '')}"
@@ -74,7 +69,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                         if response.getcode() == 200:
                             status = json.loads(response.read())
                             user = status['user']
-                            item = { 'id': status['id'], 'text': status['text'], 'timestamp': int(datetime.strptime(status['created_at'],'%a %b %d %H:%M:%S +0000 %Y').timestamp()), 'user': { 'id': user["screen_name"], 'name': user['name'], 'image': user['profile_image_url_https'] } }
+                            item = {'id': status['id'], 'text': status['text'], 'timestamp': int(datetime.strptime(status['created_at'], '%a %b %d %H:%M:%S +0000 %Y').timestamp()), 'user': {
+                                'id': user["screen_name"], 'name': user['name'], 'image': user['profile_image_url_https']}}
 
                             if 'extended_entities' in status:
                                 entities = status['extended_entities']
@@ -84,26 +80,32 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
                                     for media in entities['media']:
                                         if media['type'] == 'photo':
-                                            images.append(media['media_url_https'])
+                                            images.append(
+                                                media['media_url_https'])
 
                                     if len(images) > 0:
                                         item['image'] = images[0]
-                            
-                            return func.HttpResponse(json.dumps(item), status_code=200, headers=headers, charset='utf-8')
+
+                            if 'Origin' in req.headers:
+                                return func.HttpResponse(json.dumps(item), status_code=200, headers={['Access-Control-Allow-Origin']: req.headers['Origin']}, mimetype='application/json', charset='')
+        
+                            return func.HttpResponse(json.dumps(item), status_code=200, mimetype='application/json', charset='')
 
                     else:
-                        match = re.match("data:([\\w/\\-\\.]+);(\\w+),(.+)", image)
-                    
+                        match = re.match(
+                            "data:([\\w/\\-\\.]+);(\\w+),(.+)", image)
+
                         if match:
                             mime_type, encoding, image_data = match.groups()
-                        
+
                             if mime_type in ['image/gif', 'image/png', 'image/jpeg'] and encoding == 'base64':
                                 url = 'https://upload.twitter.com/1.1/media/upload.json'
                                 parameters = f"oauth_consumer_key={CONSUMER_KEY}&oauth_nonce={str(nonce)}&oauth_signature_method=HMAC-SHA1&oauth_timestamp={str(timestamp)}&oauth_token={access_token}&oauth_version=1.0"
                                 signature_base = f"POST&{quote(url, '')}&{quote(parameters, '')}"
                                 authorization_header = f'OAuth oauth_consumer_key="{quote(CONSUMER_KEY, "")}", oauth_nonce="{quote(str(nonce), "")}", oauth_signature="{quote(b64encode(hmac.new(("{0}&{1}".format(CONSUMER_SECRET, secret)).encode(), signature_base.encode(), sha1).digest()).decode(), "")}", oauth_signature_method="{quote("HMAC-SHA1", "")}", oauth_timestamp="{quote(str(timestamp), "")}", oauth_token="{quote(access_token, "")}", oauth_version="{quote("1.0", "")}"'
 
-                                boundary = md5(str(random.randint(0, 32000)).encode()).hexdigest()
+                                boundary = md5(
+                                    str(random.randint(0, 32000)).encode()).hexdigest()
                                 data = f'--{boundary}\r\n'.encode()
                                 data += 'Content-Disposition: form-data; name="media"; filename="rkgk"\r\n'.encode()
                                 data += f'Content-Type: {mime_type}\r\n'.encode()
@@ -121,7 +123,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                                     method='POST'))
 
                                 if response.getcode() == 200:
-                                    media_id = json.loads(response.read())['media_id_string']
+                                    media_id = json.loads(response.read())[
+                                        'media_id_string']
                                     parameters = f"media_ids={media_id}&oauth_consumer_key={CONSUMER_KEY}&oauth_nonce={str(nonce)}&oauth_signature_method=HMAC-SHA1&oauth_timestamp={str(timestamp)}&oauth_token={access_token}&oauth_version=1.0&status={quote(status, '')}"
                                     url = 'https://api.twitter.com/1.1/statuses/update.json'
                                     signature_base = f"POST&{quote(url, '')}&{quote(parameters, '')}"
@@ -137,7 +140,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                                     if response.getcode() == 200:
                                         status = json.loads(response.read())
                                         user = status['user']
-                                        item = { 'id': status['id'], 'text': status['text'], 'timestamp': int(datetime.strptime(status['created_at'],'%a %b %d %H:%M:%S +0000 %Y').timestamp()), 'user': { 'id': user["screen_name"], 'name': user['name'], 'image': user['profile_image_url_https'] } }
+                                        item = {'id': status['id'], 'text': status['text'], 'timestamp': int(datetime.strptime(status['created_at'], '%a %b %d %H:%M:%S +0000 %Y').timestamp()), 'user': {
+                                            'id': user["screen_name"], 'name': user['name'], 'image': user['profile_image_url_https']}}
 
                                         if 'extended_entities' in status:
                                             entities = status['extended_entities']
@@ -147,35 +151,43 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
                                                 for media in entities['media']:
                                                     if media['type'] == 'photo':
-                                                        images.append(media['media_url_https'])
+                                                        images.append(
+                                                            media['media_url_https'])
 
                                                 if len(images) > 0:
                                                     item['image'] = images[0]
 
-                                        return func.HttpResponse(json.dumps(item), status_code=200, headers=headers, charset='utf-8')
+                                        if 'Origin' in req.headers:
+                                            return func.HttpResponse(json.dumps(item), status_code=200, headers={['Access-Control-Allow-Origin']: req.headers['Origin']}, mimetype='application/json', charset='')
+        
+                                        return func.HttpResponse(json.dumps(item), status_code=200, mimetype='application/json', charset='')
 
-            return func.HttpResponse(status_code=400, headers=headers)
-            
+            return func.HttpResponse(status_code=400, mimetype='', charset='')
+
         elif req.method == 'GET':
-            headers['Content-Type'] = 'application/json'
-
             if req.headers.get('Content-Type') == 'application/json':
                 data = req.get_json()
-                access_token = data['access_token'] if 'access_token' in data else os.environ.get("TWITTER_OAUTH_TOKEN")
-                secret = data['secret'] if 'secret' in data else os.environ.get("TWITTER_OAUTH_TOKEN_SECRET")
+                access_token = data['access_token'] if 'access_token' in data else os.environ.get(
+                    "TWITTER_OAUTH_TOKEN")
+                secret = data['secret'] if 'secret' in data else os.environ.get(
+                    "TWITTER_OAUTH_TOKEN_SECRET")
                 query = data['query'] if 'query' in data else '#milchchan'
                 count = data['count'] if 'count' in data else 100
-                
+
             else:
-                access_token = req.params['access_token'] if 'access_token' in req.params else os.environ.get("TWITTER_OAUTH_TOKEN")
-                secret = req.params['secret'] if 'secret' in req.params else os.environ.get("TWITTER_OAUTH_TOKEN_SECRET")
+                access_token = req.params['access_token'] if 'access_token' in req.params else os.environ.get(
+                    "TWITTER_OAUTH_TOKEN")
+                secret = req.params['secret'] if 'secret' in req.params else os.environ.get(
+                    "TWITTER_OAUTH_TOKEN_SECRET")
                 query = req.params['query'] if 'query' in req.params else '#milchchan'
-                count = int(req.params['count']) if 'count' in req.params else 100
+                count = int(req.params['count']
+                            ) if 'count' in req.params else 100
 
             CONSUMER_KEY = os.environ.get("TWITTER_CONSUMER_KEY")
             CONSUMER_SECRET = os.environ.get("TWITTER_CONSUMER_SECRET")
 
-            timestamp = int(datetime.utcfromtimestamp(datetime.now(timezone.utc).timestamp()).timestamp())
+            timestamp = int(datetime.utcfromtimestamp(
+                datetime.now(timezone.utc).timestamp()).timestamp())
             nonce = ''.join([str(random.randint(0, 9)) for i in range(8)])
             result_type = 'recent'
             url = 'https://api.twitter.com/1.1/search/tweets.json'
@@ -197,7 +209,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                         user = status['user']
 
                         if not user['protected']:
-                            item = { 'id': status['id'], 'text': status['text'], 'timestamp': int(datetime.strptime(status['created_at'],'%a %b %d %H:%M:%S +0000 %Y').timestamp()), 'user': { 'id': user["screen_name"], 'name': user['name'], 'image': user['profile_image_url_https'] } }
+                            item = {'id': status['id'], 'text': status['text'], 'timestamp': int(datetime.strptime(status['created_at'], '%a %b %d %H:%M:%S +0000 %Y').timestamp()), 'user': {
+                                'id': user["screen_name"], 'name': user['name'], 'image': user['profile_image_url_https']}}
 
                             if 'extended_entities' in status:
                                 entities = status['extended_entities']
@@ -207,46 +220,40 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
                                     for media in entities['media']:
                                         if media['type'] == 'photo':
-                                            images.append(media['media_url_https'])
+                                            images.append(
+                                                media['media_url_https'])
 
                                     if len(images) > 0:
                                         item['image'] = images[0]
 
                             data.append(item)
 
-                return func.HttpResponse(json.dumps(data), status_code=200, headers=headers, charset='utf-8')
+                if 'Origin' in req.headers:
+                    return func.HttpResponse(json.dumps(data), status_code=200, headers={['Access-Control-Allow-Origin']: req.headers['Origin']}, mimetype='application/json', charset='')
+        
+                return func.HttpResponse(json.dumps(data), status_code=200, mimetype='application/json', charset='')
 
-            return func.HttpResponse(status_code=400, headers=headers)
+            return func.HttpResponse(status_code=400, mimetype='', charset='')
 
-        else:
-            headers = {}
+        elif req.method == 'OPTIONS':
+            headers = {'Access-Control-Allow-Origin': req.headers['Origin']}
+            headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
 
-            if 'Origin' in req.headers:
-                headers['Access-Control-Allow-Origin'] = req.headers['Origin']
-            else:
-                headers['Access-Control-Allow-Origin'] = '*'
-
-            headers['Access-Control-Allow-Methods'] = 'POST, GET, HEAD, OPTIONS'            
-
-            #if 'Access-Control-Request-Headers' in req.headers:
-            #    headers['Access-Control-Allow-Headers'] = req.headers['Access-Control-Request-Headers']
-            #else:
-            headers['Access-Control-Allow-Headers'] = 'Accept, Content-Type'
+            if 'Access-Control-Request-Headers' in req.headers:
+                headers['Access-Control-Allow-Headers'] = req.headers['Access-Control-Request-Headers']
 
             return func.HttpResponse(status_code=204, headers=headers, mimetype='', charset='')
 
-        #headers['Allow'] = 'GET, POST, OPTIONS'
-
-        #return func.HttpResponse(status_code=405, headers=headers)
+        return func.HttpResponse(status_code=405, headers={'Allow': 'GET, POST, OPTIONS'}, mimetype='', charset='')
 
     except Exception as e:
         logging.error(f'{e}')
 
         return func.HttpResponse(json.dumps({
-                'error': {
-                    'message': str(e),
-                    'type': type(e).__name__ }
-            }),
+            'error': {
+                'message': str(e),
+                'type': type(e).__name__}
+        }),
             status_code=400,
-            headers=headers,
-            charset='utf-8')
+            mimetype='application/json',
+            charset='')
