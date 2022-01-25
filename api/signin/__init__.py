2aae6ef769c7aa0ab206b44de1588120f247f99f
Milch <kawatan@gmail.com>
2022-01-25 18:59:44 +0900

    Update __init__.py

--- api/signin/__init__.py
+++ api/signin/__init__.py
@@ -8,34 +8,24 @@ import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    headers = {'Content-Type': 'application/json'}
-
-    if 'Origin' in req.headers:
-        headers['Access-Control-Allow-Origin'] = req.headers['Origin']
-
     try:
-        if req.method == 'POST':
-            if req.headers.get('Content-Type') == 'application/json':
-                data = req.get_json()
-                email = data.get('email')
-                password = data.get('password')
-                response = urlopen(Request(
-                        f'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={os.environ.get("FIREBASE_API_KEY")}',
-                        headers={'Content-Type': 'application/json'},
-                        data=json.dumps({'email': email, 'password': password, 'returnSecureToken': True}).encode('utf-8'),
-                        method='POST'))
+        if req.headers.get('Content-Type') == 'application/json':
+            data = req.get_json()
+            email = data.get('email')
+            password = data.get('password')
+            response = urlopen(Request(
+                    f'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={os.environ.get("FIREBASE_API_KEY")}',
+                    headers={'Content-Type': 'application/json'},
+                    data=json.dumps({'email': email, 'password': password, 'returnSecureToken': True}).encode('utf-8'),
+                    method='POST'))
 
-                if response.getcode() == 200:
-                    results = json.loads(response.read())
+            if response.getcode() == 200:
+                results = json.loads(response.read())
 
-                    return func.HttpResponse(json.dumps({'id': results['localId'], 'token': results['idToken'], 'timestamp': int(datetime.utcfromtimestamp(datetime.now(timezone.utc).timestamp()).timestamp())}), status_code=200, headers=headers, charset='utf-8')
+                return func.HttpResponse(json.dumps({'id': results['localId'], 'token': results['idToken'], 'timestamp': int(datetime.utcfromtimestamp(datetime.now(timezone.utc).timestamp()).timestamp())}), status_code=200, mimetype='application/json', charset='utf-8')
 
-            else:
-                return func.HttpResponse(status_code=400, headers=headers)
-            
-        headers['Allow'] = 'POST'
-
-        return func.HttpResponse(status_code=405, headers=headers)
+        else:
+            return func.HttpResponse(status_code=400, mimetype='', charset='')
 
     except Exception as e:
         logging.error(f'{e}')
@@ -46,5 +36,5 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                     'type': type(e).__name__ }
             }),
             status_code=400,
-            headers=headers,
+            mimetype='application/json',
             charset='utf-8')
