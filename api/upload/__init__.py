82c92db31778ccc89c6e229a445aa1eee7e594b4
Milch <kawatan@gmail.com>
2022-01-25 18:49:40 +0900

    Update __init__.py

--- api/upload/__init__.py
+++ api/upload/__init__.py
@@ -16,63 +16,58 @@ from google.cloud import storage
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        if req.method == 'POST':
-            pattern = "data:([\\w/\\-\\.]+);(\\w+),(.+)"
+        pattern = "data:([\\w/\\-\\.]+);(\\w+),(.+)"
 
-            if req.headers.get('Content-Type') == 'application/json':
-                data = req.get_json()
-                match = re.match(pattern, data.get('image'))
-                path = data.get('path')
+        if req.headers.get('Content-Type') == 'application/json':
+            data = req.get_json()
+            match = re.match(pattern, data.get('image'))
+            path = data.get('path')
 
-            else:
-                match = re.match(pattern, req.get_body.decode('utf-8'))
-                path = req.params.get('path')
+        else:
+            match = re.match(pattern, req.get_body.decode('utf-8'))
+            path = req.params.get('path')
 
-            if match:
-                mime_type, encoding, data = match.groups()
+        if match:
+            mime_type, encoding, data = match.groups()
 
-                if mime_type in ['image/apng', 'image/gif', 'image/png', 'image/jpeg', 'image/webp'] and encoding == 'base64':
-                    bucket_name = 'merkuchan.com'
+            if mime_type in ['image/apng', 'image/gif', 'image/png', 'image/jpeg', 'image/webp'] and encoding == 'base64':
+                bucket_name = 'merkuchan.com'
 
-                    if path is None:
-                        path = str(uuid4())
+                if path is None:
+                    path = str(uuid4())
 
-                    credentials = service_account.Credentials.from_service_account_info({
-                        'type': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TYPE'),
-                        'project_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID'),
-                        'private_key_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID'),
-                        'private_key': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY').replace('\\n', '\n'),
-                        'client_email': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL'),
-                        'client_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID'),
-                        'auth_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_URI'),
-                        'token_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URI'),
-                        'auth_provider_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL'),
-                        'client_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_X509_CERT_URL')
-                    })
-                    scoped_credentials = credentials.with_scopes(
-                        ['https://www.googleapis.com/auth/cloud-platform'])
-                    storage_client = storage.Client(
-                        credentials=scoped_credentials, project=scoped_credentials.project_id)
-                    bucket = storage_client.bucket(bucket_name)
-                    blob = bucket.blob(path)
+                credentials = service_account.Credentials.from_service_account_info({
+                    'type': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TYPE'),
+                    'project_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID'),
+                    'private_key_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID'),
+                    'private_key': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY').replace('\\n', '\n'),
+                    'client_email': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL'),
+                    'client_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID'),
+                    'auth_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_URI'),
+                    'token_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URI'),
+                    'auth_provider_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL'),
+                    'client_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_X509_CERT_URL')
+                })
+                scoped_credentials = credentials.with_scopes(
+                    ['https://www.googleapis.com/auth/cloud-platform'])
+                storage_client = storage.Client(
+                    credentials=scoped_credentials, project=scoped_credentials.project_id)
+                bucket = storage_client.bucket(bucket_name)
+                blob = bucket.blob(path)
 
-                    if not blob.exists():
-                        blob.upload_from_file(
-                            BytesIO(b64decode(data)), content_type=mime_type)
+                if not blob.exists():
+                    blob.upload_from_file(
+                        BytesIO(b64decode(data)), content_type=mime_type)
 
-                        return func.HttpResponse(json.dumps({
-                            'url': f'gs://{bucket_name}{urljoin("/", path)}',
-                            'timestamp': int(datetime.utcfromtimestamp(datetime.now(timezone.utc).timestamp()).timestamp())
-                        }),
-                            status_code=200,
-                            headers={
-                                'Access-Control-Allow-Origin': req.headers['Origin']} if 'Origin' in req.headers else None,
-                            mimetype='application/json',
-                            charset='utf-8')
+                    return func.HttpResponse(json.dumps({
+                        'url': f'gs://{bucket_name}{urljoin("/", path)}',
+                        'timestamp': int(datetime.utcfromtimestamp(datetime.now(timezone.utc).timestamp()).timestamp())
+                    }),
+                        status_code=200,
+                        mimetype='application/json',
+                        charset='utf-8')
 
-            return func.HttpResponse(status_code=400, mimetype='', charset='')
-
-        return func.HttpResponse(status_code=405, headers={'Allow': 'POST'}, mimetype='', charset='')
+        return func.HttpResponse(status_code=400, mimetype='', charset='')
 
     except Exception as e:
         logging.error(f'{e}')
