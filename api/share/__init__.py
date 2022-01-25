fb93f2020d46ffc010b53473e1112d0d92965e2e
Milch <kawatan@gmail.com>
2022-01-25 19:08:57 +0900

    Update __init__.py

--- api/share/__init__.py
+++ api/share/__init__.py
@@ -19,109 +19,111 @@ from google.cloud import storage
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    headers = {'Content-Type': 'application/json'}
-
-    if 'Origin' in req.headers:
-        headers['Access-Control-Allow-Origin'] = req.headers['Origin']
-
     try:
-        if req.method == 'POST':
-            if 'Authorization' in req.headers:
-                jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith('Bearer ') else req.headers['Authorization'].split('.')
+        if 'Authorization' in req.headers:
+            jwt = req.headers['Authorization'].split(' ')[1].split('.') if req.headers['Authorization'].startswith(
+                'Bearer ') else req.headers['Authorization'].split('.')
 
-                if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://securetoken.google.com/milchchan':
-                    try:
-                        response = urlopen(Request(
-                            f'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key={os.environ.get("FIREBASE_API_KEY")}',
-                            headers={'Content-Type': 'application/json'},
-                            data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
+            if json.loads(b64decode(jwt[0] + '=' * (-len(jwt[0]) % 4)))['typ'] == 'JWT' and json.loads(b64decode(jwt[1] + '=' * (-len(jwt[1]) % 4)))['iss'] == 'https://securetoken.google.com/milchchan':
+                try:
+                    response = urlopen(Request(
+                        f'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key={os.environ.get("FIREBASE_API_KEY")}',
+                        headers={'Content-Type': 'application/json'},
+                        data=json.dumps({'idToken': req.headers['Authorization']}).encode('utf-8')))
 
-                        if response.getcode() != 200:
-                            raise Exception
+                    if response.getcode() != 200:
+                        raise Exception
 
-                    except Exception:
-                        return func.HttpResponse(status_code=403, headers=headers)
-            
-            if req.headers.get('Content-Type') == 'application/json':
-                data = req.get_json()
+                except Exception:
+                    return func.HttpResponse(status_code=403, mimetype='', charset='')
 
-                if 'name' in data and data['name'] is not None and len(data['name']) > 0 and 'attributes' in data and isinstance(data['attributes'], list):
-                    id = data['id'] if 'id' in data and data['id'] is not None and len(data['id']) > 0 else str(uuid4())
-                    image = data.get('image')
-                    item = {'id': id, 'pk': id, 'name': data['name'], 'attributes': data['attributes'] }
-                        
-                    if image is not None:
-                        match = re.match("data:([\\w/\\-\\.]+);(\\w+),(.+)", image)
-                    
-                        if match:
-                            mime_type, encoding, data = match.groups()
-                        
-                            if mime_type in ['image/apng', 'image/gif', 'image/png', 'image/jpeg', 'image/webp'] and encoding == 'base64':
-                                bucket_name = 'merkuchan.com'
-                                path = str(uuid4())
-                                credentials = service_account.Credentials.from_service_account_info({
-                                    'type': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TYPE'),
-                                    'project_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID'),
-                                    'private_key_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID'),
-                                    'private_key': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY').replace('\\n', '\n'),
-                                    'client_email': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL'),
-                                    'client_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID'),
-                                    'auth_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_URI'),
-                                    'token_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URI'),
-                                    'auth_provider_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL'),
-                                    'client_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_X509_CERT_URL')
-                                })
-                                scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/cloud-platform'])
-                                storage_client = storage.Client(credentials=scoped_credentials, project=scoped_credentials.project_id)
-                                bucket = storage_client.bucket(bucket_name)
-                                blob = bucket.blob(path)
-                                
-                                if blob.exists():
-                                    return func.HttpResponse(status_code=400, headers=headers)
+        if req.headers.get('Content-Type') == 'application/json':
+            data = req.get_json()
 
-                                else:
-                                    blob.upload_from_file(BytesIO(b64decode(data)), content_type=mime_type)
-                                    item['image'] = f'gs://{bucket_name}{urljoin("/", path)}'
+            if 'name' in data and data['name'] is not None and len(data['name']) > 0 and 'attributes' in data and isinstance(data['attributes'], list):
+                id = data['id'] if 'id' in data and data['id'] is not None and len(
+                    data['id']) > 0 else str(uuid4())
+                image = data.get('image')
+                item = {'id': id, 'pk': id,
+                        'name': data['name'], 'attributes': data['attributes']}
+
+                if image is not None:
+                    match = re.match("data:([\\w/\\-\\.]+);(\\w+),(.+)", image)
+
+                    if match:
+                        mime_type, encoding, data = match.groups()
+
+                        if mime_type in ['image/apng', 'image/gif', 'image/png', 'image/jpeg', 'image/webp'] and encoding == 'base64':
+                            bucket_name = 'merkuchan.com'
+                            path = str(uuid4())
+                            credentials = service_account.Credentials.from_service_account_info({
+                                'type': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TYPE'),
+                                'project_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID'),
+                                'private_key_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID'),
+                                'private_key': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY').replace('\\n', '\n'),
+                                'client_email': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL'),
+                                'client_id': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID'),
+                                'auth_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_URI'),
+                                'token_uri': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URI'),
+                                'auth_provider_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL'),
+                                'client_x509_cert_url': os.environ.get('GOOGLE_APPLICATION_CREDENTIALS_CLIENT_X509_CERT_URL')
+                            })
+                            scoped_credentials = credentials.with_scopes(
+                                ['https://www.googleapis.com/auth/cloud-platform'])
+                            storage_client = storage.Client(
+                                credentials=scoped_credentials, project=scoped_credentials.project_id)
+                            bucket = storage_client.bucket(bucket_name)
+                            blob = bucket.blob(path)
+
+                            if blob.exists():
+                                return func.HttpResponse(status_code=400, mimetype='', charset='')
 
                             else:
-                                return func.HttpResponse(status_code=400, headers=headers)
+                                blob.upload_from_file(
+                                    BytesIO(b64decode(data)), content_type=mime_type)
+                                item['image'] = f'gs://{bucket_name}{urljoin("/", path)}'
 
                         else:
-                            return func.HttpResponse(status_code=400, headers=headers)
-                    
-                    if 'location' in data:
-                        if 'type' in data['location'] and data['location']['type'] == 'Point' and 'coordinates' in data['location']:
-                            item['location'] = {'type': data['location']['type'], 'coordinates': [data['location']['coordinates'][0], data['location']['coordinates'][1]]}
-                        else:
-                            item['location'] = {'type': 'Point', 'coordinates': [data['location']['longitude'], data['location']['latitude']]}
-                        
-                        item['geohash'] = encode_geohash(data['location']['latitude'], data['location']['longitude'])
+                            return func.HttpResponse(status_code=400, mimetype='', charset='')
 
-                    item['timestamp'] = datetime.fromtimestamp(time.time(), timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+                    else:
+                        return func.HttpResponse(status_code=400, mimetype='', charset='')
 
-                    client = CosmosClient.from_connection_string(os.environ.get('AZURE_COSMOS_DB_CONNECTION_STRING'))
-                    database = client.get_database_client('Wonderland')
-                    container = database.get_container_client('Words')
-                    container.upsert_item(item)
-                    
-                    item['timestamp'] = int(datetime.utcfromtimestamp(datetime.fromisoformat(item['timestamp'].replace('Z', '+00:00')).timestamp()).timestamp())
+                if 'location' in data:
+                    if 'type' in data['location'] and data['location']['type'] == 'Point' and 'coordinates' in data['location']:
+                        item['location'] = {'type': data['location']['type'], 'coordinates': [
+                            data['location']['coordinates'][0], data['location']['coordinates'][1]]}
+                    else:
+                        item['location'] = {'type': 'Point', 'coordinates': [
+                            data['location']['longitude'], data['location']['latitude']]}
 
-                    return func.HttpResponse(json.dumps(item), status_code=200, headers=headers, charset='utf-8')
+                    item['geohash'] = encode_geohash(
+                        data['location']['latitude'], data['location']['longitude'])
 
-            return func.HttpResponse(status_code=400, headers=headers)
+                item['timestamp'] = datetime.fromtimestamp(
+                    time.time(), timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-        headers['Allow'] = 'POST'
+                client = CosmosClient.from_connection_string(
+                    os.environ.get('AZURE_COSMOS_DB_CONNECTION_STRING'))
+                database = client.get_database_client('Wonderland')
+                container = database.get_container_client('Words')
+                container.upsert_item(item)
 
-        return func.HttpResponse(status_code=405, headers=headers)
+                item['timestamp'] = int(datetime.utcfromtimestamp(datetime.fromisoformat(
+                    item['timestamp'].replace('Z', '+00:00')).timestamp()).timestamp())
+
+                return func.HttpResponse(json.dumps(item), status_code=200, mimetype='application/json', charset='utf-8')
+
+        return func.HttpResponse(status_code=400, mimetype='', charset='')
 
     except Exception as e:
         logging.error(f'{e}')
 
         return func.HttpResponse(json.dumps({
-                'error': {
-                    'message': str(e),
-                    'type': type(e).__name__ }
-            }),
+            'error': {
+                'message': str(e),
+                'type': type(e).__name__}
+        }),
             status_code=400,
-            headers=headers,
+            mimetype='application/json',
             charset='utf-8')
