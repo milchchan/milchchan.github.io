37663d7d9eb63bf9d529e01cf48613cdf011c8f4
Milch <kawatan@gmail.com>
2022-06-21 13:45:12 +0900

    Update sharing.test.mjs

--- test/sharing.test.mjs
+++ test/sharing.test.mjs
@@ -1,32 +1,24 @@
 import assert from "assert";
-import { encodeGeohash, decodeGeohash, getNeighbors, getDistance } from "../src/geohash.mjs"
+import sinon from "sinon";
+import { share } from "../src/sharing.mjs"
 
-describe("geohash", function () {
-    it("encodeGeohash", function () {
-        assert.equal("u4pruydqqvj", encodeGeohash(57.64911, 10.40744, 11));
+global.fetch = () => { };
+global.window = {
+    btoa: value => value
+};
+
+describe("sharing", function () {
+    let fetchStub;
+
+    beforeEach(() => {
+        fetchStub = sinon.stub(global, 'fetch');
     });
-
-    it("decodeGeohash", function () {
-        const location = decodeGeohash("u4pruydqqvj");
-
-        assert.equal(57.64911, Math.round(location.latitude * 100000) / 100000);
-        assert.equal(10.40744, Math.round(location.longitude * 100000) / 100000);
+    afterEach(() => {
+        fetchStub.restore();
     });
+    it("share", async function () {
+        fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ text: "foo", author: "bar" }) }));
 
-    it("getNeighbors", function () {
-        const neighbors = getNeighbors("u4pruydqqvj");
-
-        assert.equal("u4pruydqqvm", neighbors.top);
-        assert.equal("u4pruydqquv", neighbors.bottom);
-        assert.equal("u4pruydqqvn", neighbors.right);
-        assert.equal("u4pruydqqvh", neighbors.left);
-        assert.equal("u4pruydqqvk", neighbors.topleft);
-        assert.equal("u4pruydqqvq", neighbors.topright);
-        assert.equal("u4pruydqquy", neighbors.bottomright);
-        assert.equal("u4pruydqquu", neighbors.bottomleft);
-    });
-
-    it("getDistance", function () {
-        assert.equal(838, Math.round(getDistance(59.914, 10.752, 52.523, 13.412)));
+        assert.equal(true, typeof (await share("foo", "bar")) === "object");
     });
 });
