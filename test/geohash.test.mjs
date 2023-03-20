f387387bc4eddce2bfa83ebf5cbf84d2d8ec4ad0
Masaaki Kawata <kawatan@gmail.com>
2023-03-20 14:40:08 +0900

    Refactor a code

--- test/geohash.test.mjs
+++ test/geohash.test.mjs
@@ -1,19 +1,19 @@
 import assert from "assert";
 import { encodeGeohash, decodeGeohash, getNeighbors, deg2rad, getDistance } from "../geohash.mjs"
 
-describe("geohash", function () {
-    it("encodeGeohash", function () {
+describe("geohash", () => {
+    it("encodeGeohash", () => {
         assert.equal("u4pruydqqvj", encodeGeohash(57.64911, 10.40744, 11));
     });
 
-    it("decodeGeohash", function () {
+    it("decodeGeohash", () => {
         const location = decodeGeohash("u4pruydqqvj");
 
         assert.equal(57.64911, Math.round(location.latitude * 100000) / 100000);
         assert.equal(10.40744, Math.round(location.longitude * 100000) / 100000);
     });
 
-    it("getNeighbors", function () {
+    it("getNeighbors", () => {
         const neighbors = getNeighbors("u4pruydqqvj");
 
         assert.equal("u4pruydqqvm", neighbors.top);
@@ -26,11 +26,11 @@ describe("geohash", function () {
         assert.equal("u4pruydqquu", neighbors.bottomleft);
     });
 
-    it("deg2rad", function () {
+    it("deg2rad", () => {
         assert.equal(1.57, Math.round(deg2rad(90) * 100) / 100);
     });
 
-    it("getDistance", function () {
+    it("getDistance", () => {
         assert.equal(838, Math.round(getDistance(59.914, 10.752, 52.523, 13.412)));
     });
 });
--- test/sharing.test.mjs
+++ test/sharing.test.mjs
@@ -7,7 +7,7 @@ global.window = {
     btoa: value => value
 };
 
-describe("sharing", function () {
+describe("sharing", () => {
     let fetchStub;
 
     beforeEach(() => {
@@ -16,12 +16,12 @@ describe("sharing", function () {
     afterEach(() => {
         fetchStub.restore();
     });
-    it("share", async function () {
+    it("share", async () => {
         fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve({ text: "foo", author: "bar", timestamp: 12345 }) }));
 
         assert.equal(true, typeof (await share("foo", "bar")) === "object");
     });
-    it("recent", async function () {
+    it("recent", async () => {
         fetchStub.returns(Promise.resolve({ ok: true, json: () => Promise.resolve([{ text: "foo", author: "bar", timestamp: 12345 }]) }));
 
         assert.equal(true, Array.isArray(await recent(0, 100)));
