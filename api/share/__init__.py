bc887755230ad9966484be4ed5fc89dd0cee9409
Masaaki Kawata <kawatan@gmail.com>
2023-04-23 19:51:29 +0900

    Update share API

--- api/share/__init__.py
+++ api/share/__init__.py
@@ -70,18 +70,18 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
                         if mime_type in ['image/apng', 'image/gif', 'image/png', 'image/jpeg', 'image/webp'] and encoding == 'base64':
                             bucket_name = 'merkuchan.com'
-                            path = str(uuid4())
+                            path = f'images/{str(uuid4())}'
                             credentials = service_account.Credentials.from_service_account_info({
                                 'type': os.environ['GOOGLE_APPLICATION_CREDENTIALS_TYPE'],
-                                'project_id': os.environ['GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID'],
-                                'private_key_id': os.environ['GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID'],
-                                'private_key': os.environ['GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY'].replace('\\n', '\n'),
-                                'client_email': os.environ['GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL'],
-                                'client_id': os.environ['GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID'],
+                                'project_id': os.environ['FIREBASE_CREDENTIALS_PROJECT_ID'],
+                                'private_key_id': os.environ['FIREBASE_CREDENTIALS_PRIVATE_KEY_ID'],
+                                'private_key': os.environ['FIREBASE_CREDENTIALS_PRIVATE_KEY'].replace('\\n', '\n'),
+                                'client_email': os.environ['FIREBASE_CREDENTIALS_CLIENT_EMAIL'],
+                                'client_id': os.environ['FIREBASE_CREDENTIALS_CLIENT_ID'],
                                 'auth_uri': os.environ['GOOGLE_APPLICATION_CREDENTIALS_AUTH_URI'],
                                 'token_uri': os.environ['GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URI'],
                                 'auth_provider_x509_cert_url': os.environ['GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL'],
-                                'client_x509_cert_url': os.environ['GOOGLE_APPLICATION_CREDENTIALS_CLIENT_X509_CERT_URL']
+                                'client_x509_cert_url': os.environ['FIREBASE_CREDENTIALS_CLIENT_X509_CERT_URL']
                             })
                             scoped_credentials = credentials.with_scopes(
                                 ['https://www.googleapis.com/auth/cloud-platform'])
