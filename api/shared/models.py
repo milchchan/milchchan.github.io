5be73b6f1b658c81dcf1f6db5bf5683090555177
Masaaki Kawata <kawatan@gmail.com>
2023-11-23 18:08:46 +0900

    Add models.py

---
+++ api/shared/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Like(Base):
+    __tablename__ = 'likes'
+    id = Column('id', Integer(), primary_key=True, nullable=False)
+    name = Column('name', String(255), nullable=False)
+    content = Column('content', Text(), nullable=False)
+    language = Column('language', String(3), nullable=True)
+    timestamp = Column('timestamp', DateTime(), nullable=False)
+
+
+class Attribute(Base):
+    __tablename__ = 'attributes'
+    id = Column('id', Integer(), primary_key=True, nullable=False)
+    like_id = Column('like_id', Integer(), nullable=False)
+    name = Column('name', String(255), nullable=False)
+    start = Column('start', Integer(), nullable=False)
+    end = Column('end', Integer(), nullable=False)
