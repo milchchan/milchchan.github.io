26bb7274e0ad59d5cab4e92392f0a4fd6a7fe8f1
Masaaki Kawata <kawatan@gmail.com>
2024-01-01 16:20:20 +0900

    Update upload API

--- api/upload/__init__.py
+++ api/upload/__init__.py
@@ -16,56 +16,60 @@ from google.cloud import storage
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        pattern = "data:([\\w/\\-\\.]+);(\\w+),(.+)"
+        if req.method == 'POST':
+            pattern = 'data:([\\w/\\-\\.]+);(\\w+),(.+)'
 
-        if req.headers.get('Content-Type') == 'application/json':
-            data = req.get_json()
-            match = re.match(pattern, data.get('image'))
-            path = data.get('path')
+            if req.headers.get('Content-Type') == 'application/json':
+                data = req.get_json()
+                match = re.match(pattern, data.get('image'))
+                path = data.get('path')
 
+            else:
+                match = re.match(pattern, req.get_body.decode('utf-8'))
+                path = req.params.get('path')
+
+            if match:
+                mime_type, encoding, data = match.groups()
+
+                if mime_type in ['image/apng', 'image/gif', 'image/png', 'image/jpeg', 'image/webp', 'application/zip'] and encoding == 'base64':
+                    bucket_name = 'merkuchan.com'
+
+                    if path is None:
+                        path = str(uuid4())
+
+                    credentials = service_account.Credentials.from_service_account_info({
+                        'type': os.environ['GOOGLE_APPLICATION_CREDENTIALS_TYPE'],
+                        'project_id': os.environ['GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID'],
+                        'private_key_id': os.environ['GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID'],
+                        'private_key': os.environ['GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY'].replace('\\n', '\n'),
+                        'client_email': os.environ['GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL'],
+                        'client_id': os.environ['GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID'],
+                        'auth_uri': os.environ['GOOGLE_APPLICATION_CREDENTIALS_AUTH_URI'],
+                        'token_uri': os.environ['GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URI'],
+                        'auth_provider_x509_cert_url': os.environ['GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL'],
+                        'client_x509_cert_url': os.environ['GOOGLE_APPLICATION_CREDENTIALS_CLIENT_X509_CERT_URL']
+                    })
+                    scoped_credentials = credentials.with_scopes(
+                        ['https://www.googleapis.com/auth/cloud-platform'])
+                    storage_client = storage.Client(
+                        credentials=scoped_credentials, project=scoped_credentials.project_id)
+                    bucket = storage_client.bucket(bucket_name)
+                    blob = bucket.blob(path)
+
+                    if not blob.exists():
+                        blob.upload_from_file(
+                            BytesIO(b64decode(data)), content_type=mime_type)
+
+                        return func.HttpResponse(json.dumps({
+                            'url': f'gs://{bucket_name}{urljoin("/", path)}',
+                            'timestamp': int(datetime.utcfromtimestamp(datetime.now(timezone.utc).timestamp()).timestamp())
+                        }),
+                            status_code=201,
+                            mimetype='application/json',
+                            charset='utf-8')
+                    
         else:
-            match = re.match(pattern, req.get_body.decode('utf-8'))
-            path = req.params.get('path')
-
-        if match:
-            mime_type, encoding, data = match.groups()
-
-            if mime_type in ['image/apng', 'image/gif', 'image/png', 'image/jpeg', 'image/webp'] and encoding == 'base64':
-                bucket_name = 'merkuchan.com'
-
-                if path is None:
-                    path = str(uuid4())
-
-                credentials = service_account.Credentials.from_service_account_info({
-                    'type': os.environ['GOOGLE_APPLICATION_CREDENTIALS_TYPE'],
-                    'project_id': os.environ['GOOGLE_APPLICATION_CREDENTIALS_PROJECT_ID'],
-                    'private_key_id': os.environ['GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY_ID'],
-                    'private_key': os.environ['GOOGLE_APPLICATION_CREDENTIALS_PRIVATE_KEY'].replace('\\n', '\n'),
-                    'client_email': os.environ['GOOGLE_APPLICATION_CREDENTIALS_CLIENT_EMAIL'],
-                    'client_id': os.environ['GOOGLE_APPLICATION_CREDENTIALS_CLIENT_ID'],
-                    'auth_uri': os.environ['GOOGLE_APPLICATION_CREDENTIALS_AUTH_URI'],
-                    'token_uri': os.environ['GOOGLE_APPLICATION_CREDENTIALS_TOKEN_URI'],
-                    'auth_provider_x509_cert_url': os.environ['GOOGLE_APPLICATION_CREDENTIALS_AUTH_PROVIDER_X509_CERT_URL'],
-                    'client_x509_cert_url': os.environ['GOOGLE_APPLICATION_CREDENTIALS_CLIENT_X509_CERT_URL']
-                })
-                scoped_credentials = credentials.with_scopes(
-                    ['https://www.googleapis.com/auth/cloud-platform'])
-                storage_client = storage.Client(
-                    credentials=scoped_credentials, project=scoped_credentials.project_id)
-                bucket = storage_client.bucket(bucket_name)
-                blob = bucket.blob(path)
-
-                if not blob.exists():
-                    blob.upload_from_file(
-                        BytesIO(b64decode(data)), content_type=mime_type)
-
-                    return func.HttpResponse(json.dumps({
-                        'url': f'gs://{bucket_name}{urljoin("/", path)}',
-                        'timestamp': int(datetime.utcfromtimestamp(datetime.now(timezone.utc).timestamp()).timestamp())
-                    }),
-                        status_code=200,
-                        mimetype='application/json',
-                        charset='utf-8')
+            pass
 
         return func.HttpResponse(status_code=400, mimetype='', charset='')
 
