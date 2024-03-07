0c5398ee537b531dc0bd18f6b50939f42970a893
Masaaki Kawata <kawatan@gmail.com>
2024-03-07 17:40:42 +0900

    Update models.py

--- api/shared/models.py
+++ api/shared/models.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 class Like(Base):
     __tablename__ = 'likes'
     id = Column('id', Integer(), primary_key=True, nullable=False)
-    name = Column('name', String(255), nullable=False)
+    name = Column('name', String(256), nullable=False)
     content = Column('content', Text(), nullable=False)
     language = Column('language', String(3), nullable=True)
     timestamp = Column('timestamp', DateTime(), nullable=False)
@@ -17,7 +17,7 @@ class Attribute(Base):
     __tablename__ = 'attributes'
     id = Column('id', Integer(), primary_key=True, nullable=False)
     like_id = Column('like_id', Integer(), nullable=False)
-    name = Column('name', String(255), nullable=False)
+    name = Column('name', String(256), nullable=False)
     start = Column('start', Integer(), nullable=False)
     end = Column('end', Integer(), nullable=False)
 
@@ -26,5 +26,5 @@ class Upload(Base):
     __tablename__ = 'uploads'
     id = Column('id', Integer(), primary_key=True, nullable=False)
     url = Column('url', Text(), nullable=False)
-    type = Column('type', String(255), nullable=False)
+    type = Column('type', String(1024), nullable=False)
     timestamp = Column('timestamp', DateTime(), nullable=False)
