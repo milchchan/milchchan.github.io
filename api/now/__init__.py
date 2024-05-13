8e47642820ab1c577203476b134fbee020809a4f
Masaaki Kawata <kawatan@gmail.com>
2024-05-13 19:41:14 +0900

    Add the now function

---
+++ api/now/__init__.py
@@ -0,0 +1,22 @@
+import json
+import logging
+from datetime import datetime, timezone
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        return func.HttpResponse(json.dumps({'timestamp': int(datetime.now(timezone.utc).timestamp.replace(tzinfo=timezone.utc).timestamp())}), status_code=200, mimetype='application/json', charset='utf-8')
+
+    except Exception as e:
+        logging.error(f'{e}')
+
+        return func.HttpResponse(json.dumps({
+            'error': {
+                'message': str(e),
+                'type': type(e).__name__}
+        }),
+            status_code=400,
+            mimetype='application/json',
+            charset='utf-8')
