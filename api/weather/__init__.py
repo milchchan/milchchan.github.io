eb8b04dcadf3e457cd7845eff16c9959c22ebc1b
Masaaki Kawata <kawatan@gmail.com>
2024-05-15 20:36:49 +0900

    Update weather API

--- api/weather/__init__.py
+++ api/weather/__init__.py
@@ -2,7 +2,7 @@ import time
 import json
 import logging
 import os
-from authlib.jose import jwt
+import jwt
 from urllib.request import urlopen, Request
 
 import azure.functions as func
@@ -10,22 +10,23 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        team_id = os.environ['WEATHERKIT_TEAM_ID']
-        services_id = os.environ['WEATHERKIT_SERVICES_ID']
-        private_key = os.environ['WEATHERKIT_PRIVATE_KEY']
-        key_id = os.environ['WEATHERKIT_KEY_ID']
+        team_id = 'com.milchchan.Weather'#os.environ['WEATHERKIT_TEAM_ID']
+        services_id = 'F9KSLGPDL4'#os.environ['WEATHERKIT_SERVICES_ID']
+        private_key = "-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgq+WcBNbc2LRtcmxw\njBniXBNHA2DV9Mw/+O5EOowRccGhRANCAATJt867R12JT9+4SQvBPB/CuW9ORf24\nVwp0NDb/yk+fgtzQTKbHI3tWiUeINmh7GhX10kNJt6pDzL2hDKwledqP\n-----END PRIVATE KEY-----\n"#os.environ['WEATHERKIT_PRIVATE_KEY']
+        key_id = '5MT3FB7XJP'#os.environ['WEATHERKIT_KEY_ID']
+        
         now = time.time()
         token = jwt.encode({
-            'alg': 'ES256',
-            'type':'JWT',
-            'kid': key_id,
-            'id': f'{team_id}.{services_id}'
-        }, {
             'iss': team_id,
             'iat': int(now),
             'exp': int(now + 1800),
             'sub': services_id
-        }, private_key)
+        }, private_key, algorithm='ES256', headers={
+            'alg': 'ES256',
+            'type':'JWT',
+            'kid': key_id,
+            'id': f'{team_id}.{services_id}'
+        })
         #request = Request(f'https://weatherkit.apple.com/api/v1/weather/en/{req.route_params.get('latitude')}/{req.route_params.get('longitude')}?dataSets=currentWeather', method='GET', headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'})
         return func.HttpResponse(token, status_code=200, mimetype='application/json', charset='utf-8')
     
