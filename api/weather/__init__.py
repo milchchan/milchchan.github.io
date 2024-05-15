2104a516230e3f5332766077b0f8187bc36f528b
Masaaki Kawata <kawatan@gmail.com>
2024-05-15 18:45:50 +0900

    Add weather API

---
+++ api/weather/__init__.py
@@ -0,0 +1,44 @@
+import time
+import json
+import logging
+import os
+import jwt
+from urllib.request import urlopen, Request
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        team_id = os.environ['WEATHERKIT_TEAM_ID']
+        services_id = os.environ['WEATHERKIT_SERVICES_ID']
+        private_key = os.environ['WEATHERKIT_PRIVATE_KEY']
+        key_id = os.environ['WEATHERKIT_KEY_ID']
+        now = time.time()
+        token = jwt.encode({
+            'iss': team_id,
+            'iat': int(now),
+            'exp': int(now + 1800),
+            'sub': services_id
+        }, private_key, algorithm='ES256', headers={
+            'alg': 'ES256',
+            'type':'JWT',
+            'kid': key_id,
+            'id': f'{team_id}.{services_id}'
+        })
+        request = Request(f'https://weatherkit.apple.com/api/v1/weather/en/{req.route_params.get('latitude')}/{req.route_params.get('longitude')}?dataSets=currentWeather', method='GET', headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'})
+
+        with urlopen(request) as response:
+            return func.HttpResponse(response.read().decode('utf-8'), status_code=200, mimetype='application/json', charset='utf-8')
+
+    except Exception as e:
+        logging.error(f'{e}')
+
+        return func.HttpResponse(json.dumps({
+            'error': {
+                'message': str(e),
+                'type': type(e).__name__}
+        }),
+            status_code=400,
+            mimetype='application/json',
+            charset='utf-8')
