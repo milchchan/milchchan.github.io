13743e0993c753f7a0abef2ccd3e5baccf69d637
Masaaki Kawata <kawatan@gmail.com>
2024-05-28 16:14:01 +0900

    Update generate API

--- api/generate/__init__.py
+++ api/generate/__init__.py
@@ -1,6 +1,9 @@
 import re
 import json
 import logging
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from urllib.error import HTTPError, URLError
 from datetime import datetime, timezone
 
 import azure.functions as func
@@ -9,79 +12,34 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         if req.method == 'GET':# and req.headers.get('Content-Type') == 'application/json':
-            '''
-            match = re.match('Bearer\\s(.+)', req.headers['Authorization'])
+            match = re.match('Bearer\\s(.+)', req.headers['X-Authorization'])
 
             if match:
-                pass
+                contents = []
+
+                for message in req.get_json():
+                    if message['role'] == 'system' or message['role'] == 'user':
+                        contents.append({'role': 'user', 'parts': [{'text': message['content']}]})
+                    elif message['role'] == 'assistant':
+                        contents.append({'role': 'model', 'parts': [{'text': message['content']}]})
+
+                request = Request(f'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={match.group()[1]}', data=json.dumps({
+                    'contents': contents
+                }).encode('utf-8'), method='POST', headers={'Content-Type': 'application/json'})
+
+                with urlopen(request) as response:
+                    for candidate in json.loads(response.read().decode('utf-8'))['candidates']:
+                        for part in candidate['content']['parts']:
+                            if 'text' in part:
+                                print(part['text'])
+                                match = re.match('```json(.+)```', part['text'], flags=(re.MULTILINE|re.DOTALL))
+
+                                if match:
+                                    return func.HttpResponse(json.dumps(json.loads(match.group(1))), status_code=200, mimetype='application/json', charset='utf-8')                
 
             else:
                 return func.HttpResponse(status_code=401, mimetype='', charset='')
-            '''
-            return func.HttpResponse(json.dumps({
-                'authorization': req.headers['Authorization'] if 'Authorization' in req.headers else None
-            }), status_code=200, mimetype='application/json', charset='utf-8')
             
-            '''
-            if req.headers['Authorization'].startswith('Bearer '):
-                if verify(req.headers['X-Authorization'].split(' ')[1], os.environ['AUTH0_JWKS_URL'], os.environ['AUTH0_API_AUDIENCE'], os.environ['AUTH0_ISSUER'], [os.environ['AUTH0_ALGORITHM']]) is None and verify(req.headers['X-Authorization'].split(' ')[1], os.environ['AUTH0_JWKS_URL'], os.environ['AUTH0_AUDIENCE'], os.environ['AUTH0_ISSUER'], [os.environ['AUTH0_ALGORITHM']]) is None:
-                    return func.HttpResponse(status_code=401, mimetype='', charset='')
-            else:
-                return func.HttpResponse(status_code=401, mimetype='', charset='')
-            '''
-            #data = req.get_json()
-
-            '''
-            Session = sessionmaker(bind=engine)
-            session = Session()
-
-            try:
-                attributes = []
-
-                like = Like()
-                like.name = data['name']
-                like.content = data['content']
-                like.language = data.get('language')
-                like.timestamp = datetime.now(timezone.utc)
-
-                session.add(like)
-                session.commit()
-
-                if 'attributes' in data:
-                    for item in data['attributes']:
-                        attribute = Attribute()
-                        attribute.like_id = like.id
-                        attribute.name = item['name']
-                        attribute.start = item['start']
-                        attribute.end = item['end']
-
-                        session.add(attribute)
-                        session.commit()
-
-                        attributes.append({
-                            'name': attribute.name,
-                            'start': attribute.start,
-                            'end': attribute.end
-                        })
-                
-                return func.HttpResponse(json.dumps({
-                    'id': like.id,
-                    'name': like.name,
-                    'content': like.content,
-                    'language': like.language,
-                    'attributes': attributes,
-                    'timestamp': int(like.timestamp.replace(tzinfo=timezone.utc).timestamp())
-                }), status_code=201, mimetype='application/json', charset='utf-8')
-
-            except Exception as e:
-                session.rollback()
-
-                raise e
-
-            finally:
-                session.close()
-            '''
-        
         return func.HttpResponse(status_code=400, mimetype='', charset='')
     
     except Exception as e:
