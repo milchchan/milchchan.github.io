f29d37f46dcaa31a9547c170fe65b00e40e46651
Masaaki Kawata <kawatan@gmail.com>
2024-05-28 14:26:29 +0900

    Update generate API

--- api/generate/__init__.py
+++ api/generate/__init__.py
@@ -1,71 +1,76 @@
 import json
 import logging
-import os
 from datetime import datetime, timezone
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from shared.models import Like, Attribute
 
 import azure.functions as func
 
 
-engine = create_engine(os.environ['POSTGRESQL_CONNECTION_URL'], pool_recycle=60)
-
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        if req.method == 'POST':
-            if req.headers.get('Content-Type') == 'application/json':
-                data = req.get_json()
+        if req.method == 'GET' and req.headers.get('Content-Type') == 'application/json':
+            return func.HttpResponse(json.dumps({
+                'authorization': req.headers['Authorization']
+            }), status_code=200, mimetype='application/json', charset='utf-8')
+            
+            '''
+            if req.headers['Authorization'].startswith('Bearer '):
+                if verify(req.headers['X-Authorization'].split(' ')[1], os.environ['AUTH0_JWKS_URL'], os.environ['AUTH0_API_AUDIENCE'], os.environ['AUTH0_ISSUER'], [os.environ['AUTH0_ALGORITHM']]) is None and verify(req.headers['X-Authorization'].split(' ')[1], os.environ['AUTH0_JWKS_URL'], os.environ['AUTH0_AUDIENCE'], os.environ['AUTH0_ISSUER'], [os.environ['AUTH0_ALGORITHM']]) is None:
+                    return func.HttpResponse(status_code=401, mimetype='', charset='')
+            else:
+                return func.HttpResponse(status_code=401, mimetype='', charset='')
+            '''
+            #data = req.get_json()
 
-                Session = sessionmaker(bind=engine)
-                session = Session()
+            '''
+            Session = sessionmaker(bind=engine)
+            session = Session()
 
-                try:
-                    attributes = []
+            try:
+                attributes = []
 
-                    like = Like()
-                    like.name = data['name']
-                    like.content = data['content']
-                    like.language = data.get('language')
-                    like.timestamp = datetime.now(timezone.utc)
+                like = Like()
+                like.name = data['name']
+                like.content = data['content']
+                like.language = data.get('language')
+                like.timestamp = datetime.now(timezone.utc)
 
-                    session.add(like)
-                    session.commit()
+                session.add(like)
+                session.commit()
 
-                    if 'attributes' in data:
-                        for item in data['attributes']:
-                            attribute = Attribute()
-                            attribute.like_id = like.id
-                            attribute.name = item['name']
-                            attribute.start = item['start']
-                            attribute.end = item['end']
+                if 'attributes' in data:
+                    for item in data['attributes']:
+                        attribute = Attribute()
+                        attribute.like_id = like.id
+                        attribute.name = item['name']
+                        attribute.start = item['start']
+                        attribute.end = item['end']
 
-                            session.add(attribute)
-                            session.commit()
+                        session.add(attribute)
+                        session.commit()
 
-                            attributes.append({
-                                'name': attribute.name,
-                                'start': attribute.start,
-                                'end': attribute.end
-                            })
-                    
-                    return func.HttpResponse(json.dumps({
-                        'id': like.id,
-                        'name': like.name,
-                        'content': like.content,
-                        'language': like.language,
-                        'attributes': attributes,
-                        'timestamp': int(like.timestamp.replace(tzinfo=timezone.utc).timestamp())
-                    }), status_code=201, mimetype='application/json', charset='utf-8')
+                        attributes.append({
+                            'name': attribute.name,
+                            'start': attribute.start,
+                            'end': attribute.end
+                        })
+                
+                return func.HttpResponse(json.dumps({
+                    'id': like.id,
+                    'name': like.name,
+                    'content': like.content,
+                    'language': like.language,
+                    'attributes': attributes,
+                    'timestamp': int(like.timestamp.replace(tzinfo=timezone.utc).timestamp())
+                }), status_code=201, mimetype='application/json', charset='utf-8')
 
-                except Exception as e:
-                    session.rollback()
+            except Exception as e:
+                session.rollback()
 
-                    raise e
+                raise e
 
-                finally:
-                    session.close()
+            finally:
+                session.close()
+            '''
         
         return func.HttpResponse(status_code=400, mimetype='', charset='')
     
