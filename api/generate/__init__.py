5d4501212746a205dc4ad1c27a88e4e684d1a150
Masaaki Kawata <kawatan@gmail.com>
2024-05-28 16:24:19 +0900

    Update generate API

--- api/generate/__init__.py
+++ api/generate/__init__.py
@@ -2,16 +2,13 @@ import re
 import json
 import logging
 from urllib.request import urlopen, Request
-from urllib.parse import urlencode
-from urllib.error import HTTPError, URLError
-from datetime import datetime, timezone
 
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        if req.method == 'GET' and req.headers.get('Content-Type') == 'application/json':
+        if req.method == 'POST' and req.headers.get('Content-Type') == 'application/json':
             match = re.match('Bearer\\s(.+)', req.headers['X-Authorization'])
 
             if match:
@@ -23,6 +20,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                     elif message['role'] == 'assistant':
                         contents.append({'role': 'model', 'parts': [{'text': message['content']}]})
 
+                return func.HttpResponse(json.dumps(contents), status_code=200, mimetype='application/json', charset='utf-8')                
+            
                 request = Request(f'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={match.group()[1]}', data=json.dumps({
                     'contents': contents
                 }).encode('utf-8'), method='POST', headers={'Content-Type': 'application/json'})
@@ -31,7 +30,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                     for candidate in json.loads(response.read().decode('utf-8'))['candidates']:
                         for part in candidate['content']['parts']:
                             if 'text' in part:
-                                print(part['text'])
                                 match = re.match('```json(.+)```', part['text'], flags=(re.MULTILINE|re.DOTALL))
 
                                 if match:
