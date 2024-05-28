cfb8ed27e6ea64222907940e2741d6d968410095
Masaaki Kawata <kawatan@gmail.com>
2024-05-28 14:23:28 +0900

    Update like API

--- api/like/__init__.py
+++ api/like/__init__.py
@@ -14,58 +14,57 @@ engine = create_engine(os.environ['POSTGRESQL_CONNECTION_URL'], pool_recycle=60)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        if req.method == 'POST':
-            if req.headers.get('Content-Type') == 'application/json':
-                data = req.get_json()
+        if req.method == 'POST' and req.headers.get('Content-Type') == 'application/json':
+            data = req.get_json()
 
-                Session = sessionmaker(bind=engine)
-                session = Session()
+            Session = sessionmaker(bind=engine)
+            session = Session()
 
-                try:
-                    attributes = []
+            try:
+                attributes = []
 
-                    like = Like()
-                    like.name = data['name']
-                    like.content = data['content']
-                    like.language = data.get('language')
-                    like.timestamp = datetime.now(timezone.utc)
+                like = Like()
+                like.name = data['name']
+                like.content = data['content']
+                like.language = data.get('language')
+                like.timestamp = datetime.now(timezone.utc)
 
-                    session.add(like)
-                    session.commit()
+                session.add(like)
+                session.commit()
 
-                    if 'attributes' in data:
-                        for item in data['attributes']:
-                            attribute = Attribute()
-                            attribute.like_id = like.id
-                            attribute.name = item['name']
-                            attribute.start = item['start']
-                            attribute.end = item['end']
+                if 'attributes' in data:
+                    for item in data['attributes']:
+                        attribute = Attribute()
+                        attribute.like_id = like.id
+                        attribute.name = item['name']
+                        attribute.start = item['start']
+                        attribute.end = item['end']
 
-                            session.add(attribute)
-                            session.commit()
+                        session.add(attribute)
+                        session.commit()
 
-                            attributes.append({
-                                'name': attribute.name,
-                                'start': attribute.start,
-                                'end': attribute.end
-                            })
-                    
-                    return func.HttpResponse(json.dumps({
-                        'id': like.id,
-                        'name': like.name,
-                        'content': like.content,
-                        'language': like.language,
-                        'attributes': attributes,
-                        'timestamp': int(like.timestamp.replace(tzinfo=timezone.utc).timestamp())
-                    }), status_code=201, mimetype='application/json', charset='utf-8')
+                        attributes.append({
+                            'name': attribute.name,
+                            'start': attribute.start,
+                            'end': attribute.end
+                        })
+                
+                return func.HttpResponse(json.dumps({
+                    'id': like.id,
+                    'name': like.name,
+                    'content': like.content,
+                    'language': like.language,
+                    'attributes': attributes,
+                    'timestamp': int(like.timestamp.replace(tzinfo=timezone.utc).timestamp())
+                }), status_code=201, mimetype='application/json', charset='utf-8')
 
-                except Exception as e:
-                    session.rollback()
+            except Exception as e:
+                session.rollback()
 
-                    raise e
+                raise e
 
-                finally:
-                    session.close()
+            finally:
+                session.close()
         
         return func.HttpResponse(status_code=400, mimetype='', charset='')
     
