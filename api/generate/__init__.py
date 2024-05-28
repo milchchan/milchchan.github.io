b53dc8fe4067bc2d4ca3342963c2a2ab6d1f6a96
Masaaki Kawata <kawatan@gmail.com>
2024-05-28 16:14:20 +0900

    Update generate API

--- api/generate/__init__.py
+++ api/generate/__init__.py
@@ -11,7 +11,7 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        if req.method == 'GET':# and req.headers.get('Content-Type') == 'application/json':
+        if req.method == 'GET' and req.headers.get('Content-Type') == 'application/json':
             match = re.match('Bearer\\s(.+)', req.headers['X-Authorization'])
 
             if match:
