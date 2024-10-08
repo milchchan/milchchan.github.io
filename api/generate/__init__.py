cf952d9f75ccf010d586a4f6b0ba432faa8f52fb
Masaaki Kawata <kawatan@gmail.com>
2024-10-08 21:24:14 +0900

    Update generate API

--- api/generate/__init__.py
+++ api/generate/__init__.py
@@ -62,62 +62,8 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                                     return func.HttpResponse(json.dumps(json.loads(match.group(1) if match else part['text'])), status_code=201, mimetype='application/json', charset='utf-8')
             
             elif content_type.startswith('multipart/form-data;'):
-                tts_url = os.environ.get('TTS_URL')
-                return func.HttpResponse(json.dumps({'s': tts_url}), status_code=201, mimetype='application/json', charset='utf-8')
-                    
-                if tts_url is None or len(tts_url) == 0:
-                    return func.HttpResponse(status_code=503, mimetype='', charset='')
+                return func.HttpResponse(json.dumps({'s': 'ok'}), status_code=201, mimetype='application/json', charset='utf-8')
                 
-                elif tts_url.startswith('https://'):
-                    request = Request(tts_url, headers={'Content-Type': content_type}, data=req.get_body(), method='POST')
-
-                    with urlopen(request, timeout=60.0) as response:
-                        return func.HttpResponse(response.read(), status_code=201, mimetype=response.info().get_content_type())
-                
-                elif 'boundary=' in content_type:
-                    return func.HttpResponse(json.dumps({'s': 'ok'}), status_code=201, mimetype='application/json', charset='utf-8')
-                    boundary = f'--{content_type.split('boundary=')[-1]}'.encode()
-                    audio_data = None
-                    json_data = None
-
-                    for part in req.get_body().split(boundary)[1:-1]:
-                        index = part.find(b'\r\n\r\n')
-
-                        if index >= 0:
-                            headers = part[:index].split(b'\r\n')
-                            content = part[index + 4:].strip(b'--').strip()
-                            name = None
-                            content_type = None
-                            
-                            for header in headers:
-                                if header.startswith(b'Content-Disposition'):
-                                    match = re.search(r'name="([^"]+)"(?:;\sfilename="([^"]+)")?', header.decode('utf-8'))
-
-                                    if match:
-                                        name = match.groups()[0]
-                            
-                                elif header.startswith(b'Content-Type'):
-                                    content_type = header.decode('utf-8')
-                                    content_type = content_type.split(':')[1].strip()
-
-                            if name == 'file' and content_type == 'audio/wav':
-                                audio_data = content
-                            elif name == 'data' and content_type == 'application/json':
-                                json_data = json.loads(content)
-
-                    return func.HttpResponse(audio_data, status_code=201, mimetype='audio/wav')
-                    '''
-                    if audio_data is not None and json_data is not None:
-                        with tempfile.NamedTemporaryFile() as t:
-                            with open(t.name, 'wb') as f:
-                                f.write(audio_data)
-
-                            client = Client(tts_url)
-                            output_path = client.predict(handle_file(t.name), json_data['input'], json_data['language'], json_data['temperature'] if 'temperature' in json_data else 1.0)
-                            
-                            with open(output_path, mode='rb') as f:
-                                return func.HttpResponse(f.read(), status_code=201, mimetype='audio/wav')
-                    '''
 
         return func.HttpResponse(status_code=400, mimetype='', charset='')
     
