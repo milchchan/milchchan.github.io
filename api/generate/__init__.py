2b262bab40b14c1faf4a3156ed156c768c8314e0
Milch <magicalmilchchan@gmail.com>
2024-10-13 20:29:56 +0900

    Update generate API

--- api/generate/__init__.py
+++ api/generate/__init__.py
@@ -15,52 +15,80 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             content_type = req.headers.get('Content-Type')
 
             if content_type == 'application/json':
-                api_key = None
+                llm_url = os.environ.get('LLM_URL')
 
-                if 'X-Authorization' in req.headers:
-                    match = re.match('Bearer\\s(.+)', req.headers['X-Authorization'])
+                if llm_url is None or len(llm_url) == 0:
+                    api_key = None
 
-                    if match:
-                        api_key = match.group(1)
+                    if 'X-Authorization' in req.headers:
+                        match = re.match('Bearer\\s(.+)', req.headers['X-Authorization'])
 
-                else:
-                    api_key = os.environ['GOOGLE_API_KEY']
+                        if match:
+                            api_key = match.group(1)
+
+                    else:
+                        api_key = os.environ['GOOGLE_API_KEY']
+                        
+                    if api_key is None:
+                        return func.HttpResponse(status_code=401, mimetype='', charset='')
                     
-                if api_key is None:
-                    return func.HttpResponse(status_code=401, mimetype='', charset='')
+                    else:
+                        temperature = float(req.params['temperature']) if 'temperature' in req.params else None
+                        data = req.get_json()
+                        contents = []
+
+                        if isinstance(data, dict):
+                            if 'temperature' in data:
+                                temperature = data['temperature']
+
+                            for message in data['messages']:
+                                if message['role'] == 'system' or message['role'] == 'user':
+                                    contents.append({'role': 'user', 'parts': [{'text': message['content']}]})
+                                elif message['role'] == 'assistant':
+                                    contents.append({'role': 'model', 'parts': [{'text': message['content']}]})
+
+                        elif isinstance(data, list):
+                            for message in data:
+                                if message['role'] == 'system' or message['role'] == 'user':
+                                    contents.append({'role': 'user', 'parts': [{'text': message['content']}]})
+                                elif message['role'] == 'assistant':
+                                    contents.append({'role': 'model', 'parts': [{'text': message['content']}]})
+
+                        request = Request(f'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={api_key}', data=json.dumps({'contents': contents} if temperature is None else {'contents': contents, 'generationConfig': {'temperature': temperature}}).encode('utf-8'), method='POST', headers={'Content-Type': 'application/json'})
+
+                        with urlopen(request) as response:
+                            for candidate in json.loads(response.read().decode('utf-8'))['candidates']:
+                                for part in candidate['content']['parts']:
+                                    if 'text' in part:
+                                        match = re.match('(?:```json)?(?:[^{]+)?({.+}).*(?:```)?', part['text'], flags=(re.MULTILINE|re.DOTALL))
+
+                                        return func.HttpResponse(json.dumps(json.loads(match.group(1) if match else part['text'])), status_code=201, mimetype='application/json', charset='utf-8')
                 
                 else:
-                    temperature = float(req.params['temperature']) if 'temperature' in req.params else None
-                    data = req.get_json()
-                    contents = []
+                    temperature = 1.0
+                    input_text = ''
 
-                    if isinstance(data, dict):
-                        if 'temperature' in data:
-                            temperature = data['temperature']
+                    if 'temperature' in req.get_json():
+                        temperature = data['temperature']
 
                         for message in data['messages']:
                             if message['role'] == 'system' or message['role'] == 'user':
-                                contents.append({'role': 'user', 'parts': [{'text': message['content']}]})
+                                input_text += f'<start_of_turn>user\n{message['content']}<end_of_turn>\n'
                             elif message['role'] == 'assistant':
-                                contents.append({'role': 'model', 'parts': [{'text': message['content']}]})
+                                input_text += f'<start_of_turn>model\n{message['content']}<end_of_turn>\n'
 
-                    elif isinstance(data, list):
-                        for message in data:
-                            if message['role'] == 'system' or message['role'] == 'user':
-                                contents.append({'role': 'user', 'parts': [{'text': message['content']}]})
-                            elif message['role'] == 'assistant':
-                                contents.append({'role': 'model', 'parts': [{'text': message['content']}]})
+                    if len(input_text) > 0:
+                        client = Client(llm_url, hf_token=os.environ['HF_TOKEN'])
+                        result = client.predict(input_text + '<start_of_turn>model\n', temperature, api_name='/generate')
+                        matches = re.findall(r'<start_of_turn>model\n(.+?)(?:(?:<end_of_turn>)|$)', result, re.DOTALL)
 
-                    request = Request(f'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key={api_key}', data=json.dumps({'contents': contents} if temperature is None else {'contents': contents, 'generationConfig': {'temperature': temperature}}).encode('utf-8'), method='POST', headers={'Content-Type': 'application/json'})
+                        if len(matches) > 0:
+                            match = re.match('(?:```json)?(?:[^{]+)?({.+}).*(?:```)?', matches[len(matches) - 1], flags=(re.MULTILINE|re.DOTALL))
 
-                    with urlopen(request) as response:
-                        for candidate in json.loads(response.read().decode('utf-8'))['candidates']:
-                            for part in candidate['content']['parts']:
-                                if 'text' in part:
-                                    match = re.match('(?:```json)?(?:[^{]+)?({.+}).*(?:```)?', part['text'], flags=(re.MULTILINE|re.DOTALL))
+                            return func.HttpResponse(json.dumps(json.loads(match.group(1) if match else part['text'])), status_code=201, mimetype='application/json', charset='utf-8')
 
-                                    return func.HttpResponse(json.dumps(json.loads(match.group(1) if match else part['text'])), status_code=201, mimetype='application/json', charset='utf-8')
-            
+                    return func.HttpResponse(status_code=503, mimetype='', charset='')
+
             elif content_type.startswith('multipart/form-data;'):
                 tts_url = os.environ.get('TTS_URL')
                     
