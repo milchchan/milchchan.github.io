69c8c216fc46b678b171252d4dcdd7859ebc38ac
Masaaki Kawata <kawatan@gmail.com>
2024-11-23 19:01:40 +0900

    Update logs API

--- api/logs/__init__.py
+++ api/logs/__init__.py
@@ -18,6 +18,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 order = req.params['order'] if 'order' in req.params else 'desc'
                 offset = int(req.params['offset']) if 'offset' in req.params else None
                 limit = int(req.params['limit']) if 'limit' in req.params else None
+                path = req.params['path'] if 'path' in req.params else None
                 from_date = int(req.params['from']) if 'from' in req.params else None
                 to_date = int(req.params['to']) if 'to' in req.params else None
                 geohash = req.params['geohash'] if 'geohash' in req.params else None
@@ -25,10 +26,76 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 database = client.get_database_client('Milch')
                 container = database.get_container_client('Logs')
 
-                if geohash is None:
+                if path is None:
+                    if geohash is None:
+                        if from_date is None:
+                            items = list(container.query_items(
+                                query=f'SELECT l.id, l.data, l.timestamp FROM Logs AS l WHERE l.timestamp <= @to_date ORDER BY l.timestamp {"DESC" if order == "desc" else "ASC"} OFFSET @offset LIMIT @limit',
+                                parameters=[
+                                    {'name': '@offset', 'value': 0 if offset is None else offset},
+                                    {'name': '@limit', 'value': 100 if limit is None else limit},
+                                    {'name': '@to_date', 'value': datetime.fromtimestamp(time.time() if to_date is None else to_date, timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')}
+                                ],
+                                enable_cross_partition_query=True))
+
+                        else:
+                            datetime.fromtimestamp(time.time(), timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
+                            items = list(container.query_items(
+                                query=f'SELECT l.id, l.location, l.timestamp FROM Logs AS l WHERE l.timestamp > @from_date AND l.timestamp <= @to_date ORDER BY l.timestamp {"DESC" if order == "desc" else "ASC"} OFFSET @offset LIMIT @limit',
+                                parameters=[
+                                    {'name': '@offset', 'value': 0 if offset is None else offset},
+                                    {'name': '@limit', 'value': 100 if limit is None else limit},
+                                    {'name': '@from_date', 'value': datetime.fromtimestamp(from_date, timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')},
+                                    {'name': '@to_date', 'value': datetime.fromtimestamp(time.time() if to_date is None else to_date, timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')}
+                                ],
+                                enable_cross_partition_query=True))
+
+                    else:
+                        neighbors = get_neighbors(geohash)
+
+                        if from_date is None:
+                            items = list(container.query_items(
+                                query=f"SELECT l.id, l.data, l.timestamp FROM Logs AS l WHERE l.timestamp <= @to_date AND (l.geohash LIKE CONCAT(@centergeohash, '%') OR l.geohash LIKE CONCAT(@topgeohash, '%') OR l.geohash LIKE CONCAT(@bottomgeohash, '%') OR l.geohash LIKE CONCAT(@rightgeohash, '%') OR l.geohash LIKE CONCAT(@leftgeohash, '%') OR l.geohash LIKE CONCAT(@topleftgeohash, '%') OR l.geohash LIKE CONCAT(@toprightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomrightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomleftgeohash, '%')) ORDER BY l.timestamp {'DESC' if order == 'desc' else 'ASC'} OFFSET @offset LIMIT @limit",
+                                parameters=[
+                                    {'name': '@offset', 'value': 0 if offset is None else offset},
+                                    {'name': '@limit', 'value': 100 if limit is None else limit},
+                                    {'name': '@to_date', 'value': datetime.fromtimestamp(time.time() if to_date is None else to_date, timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')},
+                                    {'name': '@centergeohash', 'value': geohash},
+                                    {'name': '@topgeohash', 'value': neighbors['top']},
+                                    {'name': '@bottomgeohash', 'value': neighbors['bottom']},
+                                    {'name': '@rightgeohash', 'value': neighbors['right']},
+                                    {'name': '@leftgeohash', 'value': neighbors['left']},
+                                    {'name': '@topleftgeohash', 'value': neighbors['topleft']},
+                                    {'name': '@toprightgeohash', 'value': neighbors['topright']},
+                                    {'name': '@bottomrightgeohash', 'value': neighbors['bottomright']},
+                                    {'name': '@bottomleftgeohash', 'value': neighbors['bottomleft']}
+                                ],
+                                enable_cross_partition_query=True))
+
+                        else:
+                            items = list(container.query_items(
+                                query=f"SELECT l.id, l.data, l.timestamp FROM Logs AS l WHERE l.timestamp > @from_date AND l.timestamp <= @to_date AND (l.geohash LIKE CONCAT(@centergeohash, '%') OR l.geohash LIKE CONCAT(@topgeohash, '%') OR l.geohash LIKE CONCAT(@bottomgeohash, '%') OR l.geohash LIKE CONCAT(@rightgeohash, '%') OR l.geohash LIKE CONCAT(@leftgeohash, '%') OR l.geohash LIKE CONCAT(@topleftgeohash, '%') OR l.geohash LIKE CONCAT(@toprightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomrightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomleftgeohash, '%')) ORDER BY l.timestamp {'DESC' if order == 'desc' else 'ASC'} OFFSET @offset LIMIT @limit",
+                                parameters=[
+                                    {'name': '@offset', 'value': 0 if offset is None else offset},
+                                    {'name': '@limit', 'value': 100 if limit is None else limit},
+                                    {'name': '@from_date', 'value': datetime.fromtimestamp(from_date, timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')},
+                                    {'name': '@to_date', 'value': datetime.fromtimestamp(time.time() if to_date is None else to_date, timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')},
+                                    {'name': '@centergeohash', 'value': geohash},
+                                    {'name': '@topgeohash', 'value': neighbors['top']},
+                                    {'name': '@bottomgeohash', 'value': neighbors['bottom']},
+                                    {'name': '@rightgeohash', 'value': neighbors['right']},
+                                    {'name': '@leftgeohash', 'value': neighbors['left']},
+                                    {'name': '@topleftgeohash', 'value': neighbors['topleft']},
+                                    {'name': '@toprightgeohash', 'value': neighbors['topright']},
+                                    {'name': '@bottomrightgeohash', 'value': neighbors['bottomright']},
+                                    {'name': '@bottomleftgeohash', 'value': neighbors['bottomleft']}
+                                ],
+                                enable_cross_partition_query=True))
+                            
+                elif geohash is None:
                     if from_date is None:
                         items = list(container.query_items(
-                            query=f'SELECT l.id, l.location, l.timestamp FROM Logs AS l WHERE l.timestamp <= @to_date ORDER BY l.timestamp {"DESC" if order == "desc" else "ASC"} OFFSET @offset LIMIT @limit',
+                            query=f'SELECT l.id, l.data, l.timestamp FROM Logs AS l WHERE l.timestamp <= @to_date ORDER BY l.timestamp {"DESC" if order == "desc" else "ASC"} OFFSET @offset LIMIT @limit',
                             parameters=[
                                 {'name': '@offset', 'value': 0 if offset is None else offset},
                                 {'name': '@limit', 'value': 100 if limit is None else limit},
@@ -39,7 +106,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                     else:
                         datetime.fromtimestamp(time.time(), timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
                         items = list(container.query_items(
-                            query=f'SELECT l.id, l.location, l.timestamp FROM Logs AS l WHERE l.timestamp > @from_date AND l.timestamp <= @to_date ORDER BY l.timestamp {"DESC" if order == "desc" else "ASC"} OFFSET @offset LIMIT @limit',
+                            query=f'SELECT l.id, l.data, l.timestamp FROM Logs AS l WHERE l.timestamp > @from_date AND l.timestamp <= @to_date ORDER BY l.timestamp {"DESC" if order == "desc" else "ASC"} OFFSET @offset LIMIT @limit',
                             parameters=[
                                 {'name': '@offset', 'value': 0 if offset is None else offset},
                                 {'name': '@limit', 'value': 100 if limit is None else limit},
@@ -53,10 +120,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
                     if from_date is None:
                         items = list(container.query_items(
-                            query=f"SELECT l.id, l.location, l.timestamp FROM Logs AS l WHERE l.timestamp <= @to_date AND (l.geohash LIKE CONCAT(@centergeohash, '%') OR l.geohash LIKE CONCAT(@topgeohash, '%') OR l.geohash LIKE CONCAT(@bottomgeohash, '%') OR l.geohash LIKE CONCAT(@rightgeohash, '%') OR l.geohash LIKE CONCAT(@leftgeohash, '%') OR l.geohash LIKE CONCAT(@topleftgeohash, '%') OR l.geohash LIKE CONCAT(@toprightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomrightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomleftgeohash, '%')) ORDER BY l.timestamp {'DESC' if order == 'desc' else 'ASC'} OFFSET @offset LIMIT @limit",
+                            query=f"SELECT l.id, l.data, l.timestamp FROM Logs AS l WHERE l.path = @path AND l.timestamp <= @to_date AND (l.geohash LIKE CONCAT(@centergeohash, '%') OR l.geohash LIKE CONCAT(@topgeohash, '%') OR l.geohash LIKE CONCAT(@bottomgeohash, '%') OR l.geohash LIKE CONCAT(@rightgeohash, '%') OR l.geohash LIKE CONCAT(@leftgeohash, '%') OR l.geohash LIKE CONCAT(@topleftgeohash, '%') OR l.geohash LIKE CONCAT(@toprightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomrightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomleftgeohash, '%')) ORDER BY l.timestamp {'DESC' if order == 'desc' else 'ASC'} OFFSET @offset LIMIT @limit",
                             parameters=[
                                 {'name': '@offset', 'value': 0 if offset is None else offset},
                                 {'name': '@limit', 'value': 100 if limit is None else limit},
+                                {'name': '@path', 'value': path},
                                 {'name': '@to_date', 'value': datetime.fromtimestamp(time.time() if to_date is None else to_date, timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')},
                                 {'name': '@centergeohash', 'value': geohash},
                                 {'name': '@topgeohash', 'value': neighbors['top']},
@@ -72,10 +140,11 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
                     else:
                         items = list(container.query_items(
-                            query=f"SELECT l.id, l.location, l.timestamp FROM Logs AS l WHERE l.timestamp > @from_date AND l.timestamp <= @to_date AND (l.geohash LIKE CONCAT(@centergeohash, '%') OR l.geohash LIKE CONCAT(@topgeohash, '%') OR l.geohash LIKE CONCAT(@bottomgeohash, '%') OR l.geohash LIKE CONCAT(@rightgeohash, '%') OR l.geohash LIKE CONCAT(@leftgeohash, '%') OR l.geohash LIKE CONCAT(@topleftgeohash, '%') OR l.geohash LIKE CONCAT(@toprightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomrightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomleftgeohash, '%')) ORDER BY l.timestamp {'DESC' if order == 'desc' else 'ASC'} OFFSET @offset LIMIT @limit",
+                            query=f"SELECT l.id, l.data, l.timestamp FROM Logs AS l WHERE l.path = @path AND l.timestamp > @from_date AND l.timestamp <= @to_date AND (l.geohash LIKE CONCAT(@centergeohash, '%') OR l.geohash LIKE CONCAT(@topgeohash, '%') OR l.geohash LIKE CONCAT(@bottomgeohash, '%') OR l.geohash LIKE CONCAT(@rightgeohash, '%') OR l.geohash LIKE CONCAT(@leftgeohash, '%') OR l.geohash LIKE CONCAT(@topleftgeohash, '%') OR l.geohash LIKE CONCAT(@toprightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomrightgeohash, '%') OR l.geohash LIKE CONCAT(@bottomleftgeohash, '%')) ORDER BY l.timestamp {'DESC' if order == 'desc' else 'ASC'} OFFSET @offset LIMIT @limit",
                             parameters=[
                                 {'name': '@offset', 'value': 0 if offset is None else offset},
                                 {'name': '@limit', 'value': 100 if limit is None else limit},
+                                {'name': '@path', 'value': path},
                                 {'name': '@from_date', 'value': datetime.fromtimestamp(from_date, timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')},
                                 {'name': '@to_date', 'value': datetime.fromtimestamp(time.time() if to_date is None else to_date, timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')},
                                 {'name': '@centergeohash', 'value': geohash},
