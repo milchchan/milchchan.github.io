5b16c2a5de6b9d8414864668b66bbf2f1c56c3ff
Masaaki Kawata <kawatan@gmail.com>
2025-01-19 13:34:10 +0900

    Update generate API

--- api/generate/__init__.py
+++ api/generate/__init__.py
@@ -46,7 +46,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                                 else:
                                     messages.append(message)
 
-                            request = Request('https://api.openai.com/v1/chat/completions', data=json.dumps({'model': os.environ['OPENAI_MODEL'], 'messages': messages, 'temperature': data['temperature']} if 'temperature' in data else {'model': os.environ['OPENAI_MODEL'], 'messages': messages}).encode('utf-8'), method='POST', headers={'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'})
+                            request = Request('https://api.openai.com/v1/chat/completions', data=json.dumps({'model': data['model'] if 'model' in data else os.environ['OPENAI_MODEL'], 'messages': messages, 'temperature': data['temperature']} if 'temperature' in data else {'model': data['model'] if 'model' in data else os.environ['OPENAI_MODEL'], 'messages': messages}).encode('utf-8'), method='POST', headers={'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'})
 
                             with urlopen(request) as response:
                                 for choice in json.loads(response.read().decode('utf-8'))['choices']:
