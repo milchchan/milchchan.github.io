d0ae12d2de52a428c3114fbfcc012ea57a52e1f4
Masaaki Kawata <kawatan@gmail.com>
2025-07-01 13:28:56 +0900

    Add mcp/capabilities API

---
+++ api/capabilities/__init__.py
@@ -0,0 +1,35 @@
+import json
+import logging
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        return func.HttpResponse(json.dumps({
+            "schema_version": "2025-03-26",
+            "tools": [
+            {
+                "name": "fetch",
+                "description": "指定されたURLからコンテンツを取得",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "url": { "type": "string" }
+                    },
+                    "required": ["url"]
+                }
+            }]
+        }), status_code=200, mimetype='application/json', charset='utf-8')
+
+    except Exception as e:
+        logging.error(f'{e}')
+
+        return func.HttpResponse(json.dumps({
+            'error': {
+                'message': str(e),
+                'type': type(e).__name__}
+        }),
+            status_code=400,
+            mimetype='application/json',
+            charset='utf-8')
