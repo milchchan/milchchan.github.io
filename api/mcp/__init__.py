9d45173894630e797d7e0065f99859c04f45f4e4
Masaaki Kawata <kawatan@gmail.com>
2025-07-05 23:43:10 +0900

    Update mcp API

--- api/mcp/__init__.py
+++ api/mcp/__init__.py
@@ -26,17 +26,17 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if method != 'invoke':
         return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'error': {'code': -32601, 'message': 'Method not found'}}), status_code=400, mimetype='application/json', charset='utf-8')
 
-    if params is None or not isinstance(params, dict) or 'tool' not in params or params['tool'] != 'news':# or 'arguments' not in params or not isinstance(params['arguments'], dict) or 'url' not in params['arguments']:
+    if params is None or not isinstance(params, dict) or 'tool' not in params or params['tool'] != 'news' or 'arguments' not in params or not isinstance(params['arguments'], dict):# or 'url' not in params['arguments']:
         return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'error': {'code': -32602, 'message': 'Invalid params'}}), status_code=400, mimetype='application/json', charset='utf-8')
     
-    #arguments = params['arguments']
+    arguments = params['arguments']
 
     try:
         cache_name = 'news'
         cached_data = get_cache(cache_name)
 
         if cached_data is None:
-            result = []
+            result = ''
 
             #with urlopen(Request(unquote(arguments['url']), method='GET')) as response:
             with urlopen(Request(unquote('https://news.yahoo.co.jp/rss/topics/top-picks.xml'), method='GET')) as response:
@@ -66,23 +66,22 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 api_key = os.environ['OPENAI_API_KEY']
 
             messages = [{'role': 'developer', 'content': system_prompt}, {'role': 'user', 'content': response_body}]
-            #request = Request('https://api.openai.com/v1/chat/completions', data=json.dumps({'model': arguments['model'] if 'model' in arguments else os.environ['OPENAI_MODEL'], 'messages': messages, 'temperature': arguments['temperature']} if 'temperature' in arguments else {'model': arguments['model'] if 'model' in arguments else os.environ['OPENAI_MODEL'], 'messages': messages}).encode('utf-8'), method='POST', headers={'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'})
-            request = Request('https://api.openai.com/v1/chat/completions', data=json.dumps({'model': os.environ['OPENAI_MODEL'], 'messages': messages}).encode('utf-8'), method='POST', headers={'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'})
-
+            request = Request('https://api.openai.com/v1/chat/completions', data=json.dumps({'model': arguments['model'] if 'model' in arguments else os.environ['OPENAI_MODEL'], 'messages': messages, 'temperature': arguments['temperature']} if 'temperature' in arguments else {'model': arguments['model'] if 'model' in arguments else os.environ['OPENAI_MODEL'], 'messages': messages}).encode('utf-8'), method='POST', headers={'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'})
+            
             with urlopen(request) as response:
                 for choice in json.loads(response.read().decode('utf-8'))['choices']:
                     if choice['message']['role'] == 'assistant':
                         match = re.match('(?:```json)?(?:[^\\[]+)?(\\[.+\\]).*(?:```)?', choice['message']['content'], flags=(re.MULTILINE|re.DOTALL))
-                        result = json.loads(match.group(1) if match else choice['message']['content'])
-                        set_cache(cache_name, json.dumps(result), expire=1800)
+                        result = f"```json\n{match.group(1) if match else choice['message']['content']}\n```"
+                        set_cache(cache_name, result, expire=1800)
                     
                     else:
                         return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'error': {'code': -32603, 'message': 'Internal error'}}), status_code=400, mimetype='application/json', charset='utf-8')
 
-            return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'result': result}), status_code=201, mimetype='application/json', charset='utf-8')
+            return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'result': {'content': [{'type': 'text', 'text': result}]}}), status_code=200, mimetype='application/json', charset='utf-8')
         
         else:
-            return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'result': cached_data}), status_code=201, mimetype='application/json', charset='utf-8')
+            return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'result': {'content': [{'type': 'text', 'text': cached_data}]}}), status_code=200, mimetype='application/json', charset='utf-8')
     
     except Exception as e:
         logging.error(f'{e}')
