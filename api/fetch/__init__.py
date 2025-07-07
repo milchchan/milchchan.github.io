5b09364ed846bdbb289684fe61e2a5b499228575
Masaaki Kawata <kawatan@gmail.com>
2025-07-07 15:10:23 +0900

    Add fetch API

---
+++ api/fetch/__init__.py
@@ -0,0 +1,69 @@
+import random
+import re
+import os
+import json
+import logging
+from urllib.parse import unquote
+from urllib.request import urlopen, Request
+from shared import FETCH_URLS, FETCH_PROMPT
+from shared.cache import get_cache, set_cache
+
+import azure.functions as func
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    if req.method == 'GET':
+        return func.HttpResponse(status_code=405, mimetype='', charset='')
+    
+    else:
+        try:
+            data = req.get_json()
+            url = FETCH_URLS[random.randrange(len(FETCH_URLS))]
+            cache_name = f'fetch/{url}'
+            cached_data = get_cache(cache_name)
+
+            if cached_data is None:
+                with urlopen(Request(unquote(url), method='GET', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'})) as response:
+                    response_body = response.read().decode('utf-8')
+
+                api_key = None
+
+                if 'X-Authorization' in req.headers:
+                    match = re.match('Bearer\\s(.+)', req.headers['X-Authorization'])
+
+                    if match:
+                        api_key = match.group(1)
+
+                else:
+                    api_key = os.environ['OPENAI_API_KEY']
+
+                messages = [{'role': 'developer', 'content': FETCH_PROMPT}, {'role': 'user', 'content': response_body}]
+                request = Request('https://api.openai.com/v1/chat/completions', data=json.dumps({'model': data['model'] if 'model' in data else os.environ['OPENAI_MODEL'], 'messages': messages, 'temperature': data['temperature'] if 'temperature' in data else 1.0}).encode('utf-8'), method='POST', headers={'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'})
+                result = ''
+                
+                with urlopen(request) as response:
+                    for choice in json.loads(response.read().decode('utf-8'))['choices']:
+                        if choice['message']['role'] == 'assistant':
+                            match = re.match('(?:```json)?(?:[^\\[]+)?(\\[.+\\]).*(?:```)?', choice['message']['content'], flags=(re.MULTILINE|re.DOTALL))
+                            result = match.group(1) if match else choice['message']['content']
+                            set_cache(cache_name, result, expire=1800)
+                        
+                        else:
+                            return func.HttpResponse(status_code=500, mimetype='', charset='')
+
+                return func.HttpResponse(json.dumps(result, ensure_ascii=False), status_code=200, mimetype='application/json', charset='utf-8')
+            
+            else:
+                return func.HttpResponse(json.dumps(cached_data, ensure_ascii=False), status_code=201, mimetype='application/json', charset='utf-8')
+        
+        except Exception as e:
+            logging.error(f'{e}')
+
+            return func.HttpResponse(json.dumps({
+                'error': {
+                    'message': str(e),
+                    'type': type(e).__name__}
+            }),
+                status_code=400,
+                mimetype='application/json',
+                charset='utf-8')
