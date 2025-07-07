6eec52151489f2d7857f814d54c55197fe9f305a
Masaaki Kawata <kawatan@gmail.com>
2025-07-07 15:10:31 +0900

    Update shared data

--- api/shared/__init__.py
+++ api/shared/__init__.py
@@ -1,2 +1,15 @@
 from shared.cache import get_cache, set_cache, scan_cache, delete_cache
 from shared.geohash import encode_geohash, decode_geohash, get_neighbors
+
+FETCH_URLS = ['https://news.yahoo.co.jp/rss/topics/top-picks.xml']
+FETCH_PROMPT = '''内容を下記の出力形式に変換してください。
+
+# 出力形式
+出力形式は以下のJSONフォーマットとします。このフォーマット以外で会話しないでください。
+```json
+[{
+    "content":  "内容",
+    "url": "<URL>",
+    "timestamp": "<ISO 8601形式>"
+}]
+```'''
