14eb77d22db2e64a1d18e3cd7fe86e15b2fcb01a
Masaaki Kawata <kawatan@gmail.com>
2025-07-11 00:01:34 +0900

    Update generate API

--- api/generate/__init__.py
+++ api/generate/__init__.py
@@ -63,10 +63,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
                             if 'tools' not in data:
                                 data['tools'] = [{
-                                    'type': 'mcp',
-                                    'server_label': 'milchchan-mcp',
-                                    'server_url': "https://milchchan.com/api/mcp",
-                                    'require_approval': 'never'
+                                    "type": "mcp",
+                                    "server_label": "milchchan-mcp",
+                                    "server_url": "https://milchchan.com/api/mcp",
+                                    "require_approval": "never"
                                 }]
 
                             with urlopen(Request('https://api.openai.com/v1/responses', data=json.dumps({'model': data['model'] if 'model' in data else os.environ['OPENAI_MODEL'], 'input': messages, 'temperature': data['temperature'] if 'temperature' in data else 1.0, 'tools': data['tools']}).encode('utf-8'), method='POST', headers={'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'})) as response:
