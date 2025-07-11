bd1b2e8221599e96f31c6f7995b08c228771f6db
Masaaki Kawata <kawatan@gmail.com>
2025-07-11 13:38:13 +0900

    Update shared data

--- api/shared/__init__.py
+++ api/shared/__init__.py
@@ -1,7 +1,10 @@
 from shared.cache import get_cache, set_cache, scan_cache, delete_cache
 from shared.geohash import encode_geohash, decode_geohash, get_neighbors
 
-FETCH_URLS = ['https://news.yahoo.co.jp/rss/topics/top-picks.xml']
+FETCH_URLS = [
+    'https://hnrss.org/best',
+    'https://news.yahoo.co.jp/rss/topics/top-picks.xml'
+]
 FETCH_PROMPT = '''内容を下記の出力形式に変換してください。
 
 # 出力形式
