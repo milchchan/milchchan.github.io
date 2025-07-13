f744369469aa11365e3550d7f0c3e17e57e7e745
Masaaki Kawata <kawatan@gmail.com>
2025-07-14 07:43:16 +0900

    Update shared data

--- api/shared/__init__.py
+++ api/shared/__init__.py
@@ -5,6 +5,7 @@ FETCH_URLS = [
     'https://hnrss.org/best',
     'https://news.yahoo.co.jp/rss/topics/top-picks.xml',
     'https://pc.watch.impress.co.jp/data/rss/1.0/pcw/feed.rdf',
+    'https://game.watch.impress.co.jp/data/rss/1.0/gmw/feed.rdf',
     'https://taisy0.com/feed'
 ]
 FETCH_PROMPT = '''内容を下記の出力形式に変換してください。
