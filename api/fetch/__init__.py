445b6c2af364753d39954567174d13f7b0302e9f
Masaaki Kawata <kawatan@gmail.com>
2025-07-22 22:02:23 +0900

    Update fetch API

--- api/fetch/__init__.py
+++ api/fetch/__init__.py
@@ -1,12 +1,13 @@
 import random
 import re
+import uuid
 import os
 import json
 import logging
 from datetime import datetime, time, timedelta, timezone
 from urllib.parse import unquote
 from urllib.request import urlopen, Request
-from shared import FETCH_URLS, FETCH_PROMPT
+from shared import FETCH_URLS, FETCH_PROMPT, TRANSFORM_SYSTEM_PROMPT, TRANSFORM_USER_PROMPT
 from shared.cache import get_cache, set_cache, scan_cache
 
 import azure.functions as func
@@ -68,7 +69,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             if cached_data is not None:
                 cached_data = json.loads(cached_data)
 
-                if 'timestamp' in cached_data and cached_data['timestamp'] >= int((datetime.now(timezone.utc) - timedelta(hours=1)).timestamp()):
+                if 'timestamp' in cached_data and cached_data['timestamp'] >= int((datetime.now(timezone.utc) - timedelta(hours=6)).timestamp()):
                     return func.HttpResponse(json.dumps(cached_data['data'], ensure_ascii=False), status_code=201, mimetype='application/json', charset='utf-8')
 
             with urlopen(Request(unquote(url), method='GET', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'})) as response:
@@ -84,18 +85,36 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
             else:
                 api_key = os.environ['OPENAI_API_KEY']
-
-            messages = [{'role': 'developer', 'content': FETCH_PROMPT}, {'role': 'user', 'content': response_body}]
-            request = Request('https://api.openai.com/v1/chat/completions', data=json.dumps({'model': os.environ['OPENAI_MODEL'] if model is None else model, 'messages': messages, 'temperature': temperature}).encode('utf-8'), method='POST', headers={'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'})
             
-            with urlopen(request) as response:
-                for choice in json.loads(response.read().decode('utf-8'))['choices']:
+            with urlopen(Request('https://api.openai.com/v1/chat/completions', data=json.dumps({'model': os.environ['OPENAI_MODEL'] if model is None else model, 'messages': [{'role': 'developer', 'content': FETCH_PROMPT}, {'role': 'user', 'content': response_body}], 'temperature': temperature}).encode('utf-8'), method='POST', headers={'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'})) as response1:
+                for choice in json.loads(response1.read().decode('utf-8'))['choices']:
                     if choice['message']['role'] == 'assistant':
                         match = re.match('(?:```json)?(?:[^\\[]+)?(\\[.+\\]).*(?:```)?', choice['message']['content'], flags=(re.MULTILINE|re.DOTALL))
-                        items = json.loads(match.group(1) if match else choice['message']['content'])
-                        set_cache(cache_name, json.dumps({'data': items, 'timestamp': int(datetime.combine(datetime.now(timezone.utc).date(), time(0, 0), tzinfo=timezone.utc).timestamp())}, ensure_ascii=False), expire=86400)
+                        items1 = json.loads(match.group(1) if match else choice['message']['content'])
+                        items2 = []
+
+                        for item in items1:
+                            identifier = str(uuid.uuid4())
+                            item['id'] = identifier
+                            items2.append({'id': identifier, 'content': item['content']})
+
+                        with urlopen(Request('https://api.openai.com/v1/chat/completions', data=json.dumps({'model': os.environ['OPENAI_MODEL'] if model is None else model, 'messages': [{'role': 'developer', 'content': TRANSFORM_SYSTEM_PROMPT}, {'role': 'user', 'content': f'{TRANSFORM_USER_PROMPT}\n```json\n{json.dumps(items2, ensure_ascii=False)}\n```'}], 'temperature': temperature}).encode('utf-8'), method='POST', headers={'Authorization': f'Bearer {api_key}', 'Content-Type': 'application/json'})) as response2:
+                            for choice in json.loads(response2.read().decode('utf-8'))['choices']:
+                                if choice['message']['role'] == 'assistant':
+                                    match = re.match('(?:```json)?(?:[^\\[]+)?(\\[.+\\]).*(?:```)?', choice['message']['content'], flags=(re.MULTILINE|re.DOTALL))
+                                    items3 = json.loads(match.group(1) if match else choice['message']['content'])
+                            
+                                    for item1 in items1:
+                                        for item2 in items3:
+                                            if item1['id'] == item2['id']:
+                                                item1['comment'] = item2['content']
+                                                item1['states'] = item2['states']
+
+                                                break
                         
-                        return func.HttpResponse(json.dumps(items, ensure_ascii=False), status_code=201, mimetype='application/json', charset='utf-8')
+                                    set_cache(cache_name, json.dumps({'data': items1, 'timestamp': int(datetime.combine(datetime.now(timezone.utc).date(), time(0, 0), tzinfo=timezone.utc).timestamp())}, ensure_ascii=False), expire=86400)
+                        
+                                    return func.HttpResponse(json.dumps(items1, ensure_ascii=False), status_code=201, mimetype='application/json', charset='utf-8')
                     
             return func.HttpResponse(status_code=500, mimetype='', charset='')
         
