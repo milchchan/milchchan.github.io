cd8ebb5ec23bcbdbeea8090750a08c2446754791
Masaaki Kawata <kawatan@gmail.com>
2025-07-29 20:48:49 +0900

    Update mcp API

--- api/mcp/__init__.py
+++ api/mcp/__init__.py
@@ -1,6 +1,10 @@
+import time
 import json
 import logging
-from datetime import datetime, time, timedelta, timezone
+import os
+import jwt
+from datetime import datetime, time, timezone
+from urllib.request import urlopen, Request
 from shared.cache import get_cache, scan_cache
 
 import azure.functions as func
@@ -61,6 +65,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                                 },
                                 'required': []
                             }
+                        },
+                        {
+                            'name': 'weather',
+                            'description': 'Retrieves current weather',
+                            'inputSchema': {
+                                'type': 'object',
+                                'properties': {
+                                    'latitude': {
+                                        'type': 'number',
+                                        'minimum': -90,
+                                        'maximum': 90,
+                                        'description': 'Latitude'
+                                    },
+                                    'longitude': {
+                                        'type': 'number',
+                                        'minimum': -180,
+                                        'maximum': 180,
+                                        'description': 'Longitude'
+                                    }
+                                },
+                                'required': ['latitude', 'longitude']
+                            }
                         }]
                 }}), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
         elif identifier is None:
@@ -68,31 +94,64 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         elif method != 'tools/call':
             return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'error': {'code': -32601, 'message': 'Method not found'}}), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
 
-        if params is None or not isinstance(params, dict) or 'name' not in params or params['name'] != 'news' or 'arguments' not in params or not isinstance(params['arguments'], dict):
+        if params is None or not isinstance(params, dict) or 'name' not in params or 'arguments' not in params or not isinstance(params['arguments'], dict):
             return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'error': {'code': -32602, 'message': 'Invalid params'}}), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
         
         arguments = params['arguments']
-        limit = int(arguments['limit']) if 'limit' in arguments else 10
-        merged_data = []
-        
-        try:
-            for cache_name in scan_cache(f'fetch/*'):
-                cached_data = json.loads(get_cache(cache_name))
+
+        if params['name'] == 'news':
+            limit = int(arguments['limit']) if 'limit' in arguments else 10
+            merged_data = []
+            
+            try:
+                for cache_name in scan_cache(f'fetch/*'):
+                    cached_data = json.loads(get_cache(cache_name))
+                    
+                    if isinstance(cached_data, dict) and 'data' in cached_data and 'timestamp' in cached_data and isinstance(cached_data['data'], list):
+                        for item in cached_data['data']:
+                            if isinstance(item, dict) and 'content' in item and 'url' in item and 'timestamp' in item:
+                                merged_data.append({'content': item['content'], 'url': item['url'], 'timestamp': datetime.combine(datetime.now(timezone.utc).date(), time(0, 0), tzinfo=timezone.utc) if item['timestamp'] is None else datetime.fromisoformat(item['timestamp'].replace('Z', '+00:00'))})
+
+                recent_data = sorted(merged_data, key=lambda x: x['timestamp'], reverse=True)
+                recent_data = recent_data[:limit]
                 
-                if isinstance(cached_data, dict) and 'data' in cached_data and 'timestamp' in cached_data and isinstance(cached_data['data'], list):
-                    for item in cached_data['data']:
-                        if isinstance(item, dict) and 'content' in item and 'url' in item and 'timestamp' in item:
-                            merged_data.append({'content': item['content'], 'url': item['url'], 'timestamp': datetime.combine(datetime.now(timezone.utc).date(), time(0, 0), tzinfo=timezone.utc) if item['timestamp'] is None else datetime.fromisoformat(item['timestamp'].replace('Z', '+00:00'))})
+                for item in recent_data:
+                    item['timestamp'] = item['timestamp'].strftime('%Y-%m-%dT%H:%M:%SZ')
+                
+                return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'result': {'content': [{'type': 'text', 'text': f'```json\n{json.dumps(recent_data, ensure_ascii=False)}\n```'}], 'isError': False}}, ensure_ascii=False), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
+            
+            except Exception as e:
+                logging.error(f'{e}')
 
-            recent_data = sorted(merged_data, key=lambda x: x['timestamp'], reverse=True)
-            recent_data = recent_data[:limit]
-            
-            for item in recent_data:
-                item['timestamp'] = item['timestamp'].strftime('%Y-%m-%dT%H:%M:%SZ')
-            
-            return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'result': {'content': [{'type': 'text', 'text': f'```json\n{json.dumps(recent_data, ensure_ascii=False)}\n```'}], 'isError': False}}, ensure_ascii=False), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
+                return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'error': {'code': -32603, 'message': 'Internal error', 'data': str(e)}}), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
+
+        elif params['name'] == 'weather':
+            try:
+                team_id = os.environ['WEATHERKIT_TEAM_ID']
+                services_id = os.environ['WEATHERKIT_SERVICES_ID']
+                private_key = os.environ['WEATHERKIT_PRIVATE_KEY'].replace('\\n', '\n')
+                key_id = os.environ['WEATHERKIT_KEY_ID']
+                now = time.time()
+                token = jwt.encode({
+                    'iss': team_id,
+                    'iat': int(now),
+                    'exp': int(now + 1800),
+                    'sub': services_id
+                }, private_key, algorithm='ES256', headers={
+                    'alg': 'ES256',
+                    'type':'JWT',
+                    'kid': key_id,
+                    'id': f'{team_id}.{services_id}'
+                })
+                request = Request(f'https://weatherkit.apple.com/api/v1/weather/en/{arguments['latitude']}/{arguments['longitude']}?dataSets=currentWeather', method='GET', headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'})
+                
+                with urlopen(request) as response:
+                    return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'result': {'content': [{'type': 'text', 'text': f'```json\n{json.dumps(json.loads(response.read()), ensure_ascii=False)}\n```'}], 'isError': False}}, ensure_ascii=False), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
+                    
+            except Exception as e:
+                logging.error(f'{e}')
+
+                return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'error': {'code': -32603, 'message': 'Internal error', 'data': str(e)}}), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
         
-        except Exception as e:
-            logging.error(f'{e}')
-
-            return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'error': {'code': -32603, 'message': 'Internal error', 'data': str(e)}}), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
+        else:
+            return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'error': {'code': -32602, 'message': 'Invalid params'}}), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
