d7372119a59d2755cc4065c6ba3ae185a8baf562
Masaaki Kawata <kawatan@gmail.com>
2025-07-29 22:45:01 +0900

    Update mcp API

--- api/mcp/__init__.py
+++ api/mcp/__init__.py
@@ -144,8 +144,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                     'id': f'{team_id}.{services_id}'
                 })
                 
-                with urlopen(Request(f'https://weatherkit.apple.com/api/v1/weather/en/{str(arguments["latitude"])}/{str(arguments["longitude"])}?dataSets=currentWeather', method='GET', headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'})) as response:
-                    return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'result': {'content': [{'type': 'text', 'text': f'```json\n{response.read().decode("utf-8")}\n```'}], 'isError': False}}, ensure_ascii=False), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
+                return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'result': {'content': [{'type': 'text', 'text': f'晴れ'}], 'isError': False}}, ensure_ascii=False), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
+                #with urlopen(Request(f'https://weatherkit.apple.com/api/v1/weather/en/{str(arguments["latitude"])}/{str(arguments["longitude"])}?dataSets=currentWeather', method='GET', headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'})) as response:
+                #    return func.HttpResponse(json.dumps({'jsonrpc': '2.0', 'id': identifier, 'result': {'content': [{'type': 'text', 'text': f'```json\n{response.read().decode("utf-8")}\n```'}], 'isError': False}}, ensure_ascii=False), status_code=200, headers={'MCP-Protocol-Version': SUPPORTED_VERSION}, mimetype='application/json', charset='utf-8')
                     
             except Exception as e:
                 logging.error(f'{e}')
