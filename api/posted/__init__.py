90f327ea5275d5825b2223de5dbdc3a278f3f5b9
Masaaki Kawata <kawatan@gmail.com>
2025-08-29 20:32:44 +0900

    Update posted API

--- api/posted/__init__.py
+++ api/posted/__init__.py
@@ -1,8 +1,13 @@
+import io
 import re
 import json
 import logging
 import os
+import boto3
+import botocore
+from uuid import uuid4
 from datetime import datetime
+from urllib.request import urlopen, Request
 
 import azure.functions as func
 from azure.cosmos.cosmos_client import CosmosClient
@@ -29,8 +34,63 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                     for frame in animation:
                         frame['url'] = f"https://static.milchchan.com/{frame['id']}"
 
-                    #if index > 0:
-                    #    animation[0]['url']
+                    if index > 0:
+                        animation[:] = animation[:1]
+                        api_url = 'https://milchchan-prism.hf.space/gradio_api'
+                        session = uuid4().hex[:10]
+                        
+                        with urlopen(Request(api_url + '/queue/join', data=json.dumps({
+                            'data': [{'path': animation[0]['url'], 'meta': {'_type': 'gradio.FileData'}}],
+                            'event_data': None,
+                            'fn_index': 0,
+                            'session_hash': session
+                        }).encode(), headers={'Authorization': f"Bearer {os.environ['HF_TOKEN']}", 'Content-Type': 'application/json'})) as response:
+                            event_id = json.loads(response.read().decode('utf-8'))['event_id']
+                        
+                        with urlopen(Request(f'{api_url}/queue/data?session_hash={session}', headers={'Authorization': f"Bearer {os.environ['HF_TOKEN']}", 'Accept': 'text/event-stream'})) as response:
+                            for raw in iter(lambda: response.readline() or None, None):
+                                if not raw: # EOF
+                                    break
+
+                                line = raw.decode('utf-8').strip()
+
+                                if line.startswith('data:'):
+                                    msg = json.loads(line[5:].lstrip())
+                                    msg_type = msg.get('msg')
+
+                                    if msg_type == 'heartbeat':
+                                        continue
+                                    elif msg_type == 'queue_full' or msg_type == 'unexpected_error':
+                                        break
+                                    elif msg_type == 'process_completed':
+                                        if msg['event_id'] == event_id and 'data' in msg['output']:
+                                            urls = []
+                                            s3 = boto3.client(service_name='s3', endpoint_url=os.environ['S3_ENDPOINT_URL'], aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'], aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], region_name='auto')
+
+                                            for item in msg['output']['data'][0]:
+                                                if 'image' in item:
+                                                    urls.append(f"{api_url}/file={item['image']['path']}")
+
+                                                    with urlopen(Request(urls[index])) as r:
+                                                        content_type = r.headers.get_content_type()
+                                                        layer_identifier = str(uuid4())
+                                                        file_is_exists = True
+                                                        
+                                                        try:
+                                                            s3.head_object(Bucket='uploads', Key=layer_identifier)
+                                                        except botocore.exceptions.ClientError as e:
+                                                            if e.response['Error']['Code'] == '404':
+                                                                file_is_exists = False
+                                                            else:
+                                                                raise
+
+                                                        if file_is_exists:
+                                                            return func.HttpResponse(status_code=409, mimetype='', charset='')
+                                                        
+                                                        with io.BytesIO(r.read()) as buffer:
+                                                            s3.upload_fileobj(buffer, 'uploads', layer_identifier, ExtraArgs={'ContentType': content_type})
+
+                                                        animation.append({'id': layer_identifier, 'type': content_type})
 
             container.upsert_item(item)
 
