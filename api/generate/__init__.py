c8125c1c799ae5031b0860c7e5790439ddbfb63c
Masaaki Kawata <kawatan@gmail.com>
2026-05-01 23:19:58 +0900

    Refactor multipart form-data handling in TTS API integration

--- api/generate/__init__.py
+++ api/generate/__init__.py
@@ -254,91 +254,93 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
                 if tts_source is None or len(tts_source) == 0:
                     return func.HttpResponse(status_code=503, mimetype='', charset='')
                 
-                elif re.match(r'https?://', tts_source) is None:
-                    if 'boundary=' in content_type:
-                        boundary = f'--{content_type.split("boundary=")[-1]}'.encode()
-                        audio_data = None
-                        json_data = None
+                if 'boundary=' in content_type:
+                    boundary = f'--{content_type.split("boundary=")[-1]}'.encode()
+                    audio_data = None
+                    json_data = None
 
-                        for part in req.get_body().split(boundary)[1:-1]:
-                            index = part.find(b'\r\n\r\n')
+                    for part in req.get_body().split(boundary)[1:-1]:
+                        index = part.find(b'\r\n\r\n')
 
-                            if index >= 0:
-                                headers = part[:index].split(b'\r\n')
-                                content = part[index + 4:].strip(b'--').strip()
-                                name = None
-                                filename = None
-                                content_type = None
-                                
-                                for header in headers:
-                                    if header.startswith(b'Content-Disposition'):
-                                        match = re.search(r'name="([^"]+)"(?:;\sfilename="([^"]+)")?', header.decode('utf-8'))
+                        if index >= 0:
+                            headers = part[:index].split(b'\r\n')
+                            content = part[index + 4:].strip(b'--').strip()
+                            name = None
+                            filename = None
+                            content_type = None
+                            
+                            for header in headers:
+                                if header.startswith(b'Content-Disposition'):
+                                    match = re.search(r'name="([^"]+)"(?:;\sfilename="([^"]+)")?', header.decode('utf-8'))
 
-                                        if match:
-                                            name, filename = match.groups()
-                                
-                                    elif header.startswith(b'Content-Type'):
-                                        content_type = header.decode('utf-8').split(':')[1].strip()
+                                    if match:
+                                        name, filename = match.groups()
+                            
+                                elif header.startswith(b'Content-Type'):
+                                    content_type = header.decode('utf-8').split(':')[1].strip()
 
-                                if name == 'file' and filename is not None and content_type in ['audio/wav', 'audio/x-wav']:
-                                    audio_data = (filename, content)
-                                elif name == 'data':
-                                    json_data = json.loads(content)
+                            if name == 'file' and filename is not None and content_type in ['audio/wav', 'audio/x-wav']:
+                                audio_data = (filename, content)
+                            elif name == 'data':
+                                json_data = json.loads(content)
 
-                        if audio_data is not None and json_data is not None:
+                    if audio_data is not None and json_data is not None:
+                        if re.match(r'https?://', tts_source) is None:
                             api_url = f"https://{tts_source.replace('/', '-').lower()}.hf.space/gradio_api"
-                            session = uuid4().hex[:10]
-                            boundary = '----gradioBoundary'
-                            data = f'--{boundary}\r\n'.encode()
-                            data += f'Content-Disposition: form-data; name="files"; filename="{os.path.basename(audio_data[0])}"\r\n'.encode()
-                            data += f'Content-Type: audio/wav\r\n\r\n'.encode()
-                            data += audio_data[1]
-                            data += f'\r\n--{boundary}--\r\n'.encode()
+                            bearer_token = os.environ['HF_TOKEN']
+                        else:
+                            api_url = tts_source
+                            bearer_token = None
+                        
+                        user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0'
+                        session = uuid4().hex[:10]
+                        boundary = '----gradioBoundary'
+                        data = f'--{boundary}\r\n'.encode()
+                        data += f'Content-Disposition: form-data; name="files"; filename="{os.path.basename(audio_data[0])}"\r\n'.encode()
+                        data += f'Content-Type: audio/wav\r\n\r\n'.encode()
+                        data += audio_data[1]
+                        data += f'\r\n--{boundary}--\r\n'.encode()
 
-                            with urlopen(Request(api_url + '/upload', data=data, headers={'Content-Type': f'multipart/form-data; boundary={boundary}'}, method='POST')) as response:
-                                path = json.loads(response.read().decode('utf-8'))[0]
-                            
-                            with urlopen(Request(api_url + '/queue/join', data=json.dumps({
-                                'data': [json_data['input'], json_data['language'], {'path': path, 'meta': {'_type': 'gradio.FileData'}}, None, json_data['temperature'] if 'temperature' in json_data else 1.0],
-                                'event_data': None,
-                                'fn_index': 0,
-                                'session_hash': session
-                            }).encode(), headers={'Authorization': f"Bearer {os.environ['HF_TOKEN']}", 'Content-Type': 'application/json'})) as response:
-                                event_id = json.loads(response.read().decode('utf-8'))['event_id']
-                            
-                            path = None
+                        with urlopen(Request(api_url + '/upload', data=data, headers={'User-Agent': user_agent, 'Content-Type': f'multipart/form-data; boundary={boundary}'}, method='POST')) as response:
+                            path = json.loads(response.read().decode('utf-8'))[0]
+                        
+                        with urlopen(Request(api_url + '/queue/join', data=json.dumps({
+                            'data': [json_data['input'], json_data['language'], {'path': path, 'meta': {'_type': 'gradio.FileData'}}, None, json_data['temperature'] if 'temperature' in json_data else 1.0],
+                            'event_data': None,
+                            'fn_index': 0,
+                            'session_hash': session
+                        }).encode(), headers={'User-Agent': user_agent, 'Content-Type': 'application/json'} if bearer_token is None else {'Authorization': f"Bearer {bearer_token}", 'User-Agent': user_agent, 'Content-Type': 'application/json'})) as response:
+                            event_id = json.loads(response.read().decode('utf-8'))['event_id']
+                        
+                        path = None
 
-                            with urlopen(Request(f'{api_url}/queue/data?session_hash={session}', headers={'Authorization': f"Bearer {os.environ['HF_TOKEN']}", 'Accept': 'text/event-stream'})) as response:
-                                for raw in iter(lambda: response.readline() or None, None):
-                                    if not raw: # EOF
+                        with urlopen(Request(f'{api_url}/queue/data?session_hash={session}', headers={'Accept': 'text/event-stream', 'User-Agent': user_agent}} if bearer_token is None else {'Authorization': f"Bearer {bearer_token}", 'Accept': 'text/event-stream', 'User-Agent': user_agent})) as response:
+                            for raw in iter(lambda: response.readline() or None, None):
+                                if not raw: # EOF
+                                    break
+
+                                line = raw.decode('utf-8').strip()
+
+                                if line.startswith('data:'):
+                                    msg = json.loads(line[5:].lstrip())
+                                    msg_type = msg.get('msg')
+
+                                    if msg_type == 'heartbeat':
+                                        continue
+                                    elif msg_type == 'queue_full' or msg_type == 'unexpected_error':
+                                        break
+                                    elif msg_type == 'process_completed':
+                                        if msg['event_id'] == event_id and 'data' in msg['output']:
+                                            path = msg['output']['data'][0]['path']
+                                        
                                         break
 
-                                    line = raw.decode('utf-8').strip()
-
-                                    if line.startswith('data:'):
-                                        msg = json.loads(line[5:].lstrip())
-                                        msg_type = msg.get('msg')
-
-                                        if msg_type == 'heartbeat':
-                                            continue
-                                        elif msg_type == 'queue_full' or msg_type == 'unexpected_error':
-                                            break
-                                        elif msg_type == 'process_completed':
-                                            if msg['event_id'] == event_id and 'data' in msg['output']:
-                                                path = msg['output']['data'][0]['path']
-                                            
-                                            break
-
-                            if path is None:
-                                return func.HttpResponse(status_code=503, mimetype='', charset='')
-                            else:
-                                with urlopen(Request(f'{api_url}/file={path}')) as response:
-                                    return func.HttpResponse(response.read(), status_code=200, mimetype='audio/wav')
-
-                else:
-                    with urlopen(Request(tts_source, headers={'Content-Type': content_type}, data=req.get_body(), method='POST'), timeout=60.0) as response:
-                        return func.HttpResponse(response.read(), status_code=200, mimetype=response.info().get_content_type())
-                    
+                        if path is None:
+                            return func.HttpResponse(status_code=503, mimetype='', charset='')
+                        else:
+                            with urlopen(Request(f'{api_url}/file={path}', headers={'User-Agent': user_agent})) as response:
+                                return func.HttpResponse(response.read(), status_code=200, mimetype='audio/wav')
+    
         return func.HttpResponse(status_code=400, mimetype='', charset='')
     
     except Exception as e:
