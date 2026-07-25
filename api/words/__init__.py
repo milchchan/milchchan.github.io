06c4abe2cdf5ca1d3639b71275a89572612bc6bf
Masaaki Kawata <kawatan@gmail.com>
2026-07-25 18:00:23 +0900

    Add words API

---
+++ api/words/__init__.py
@@ -0,0 +1,90 @@
+import json
+import logging
+import os
+import certifi
+from datetime import timezone
+from urllib.parse import urlparse
+from sqlalchemy import create_engine, desc
+from sqlalchemy.orm import sessionmaker
+from shared.models import Word
+from shared.cache import get_cache, set_cache
+
+import azure.functions as func
+
+
+engine = create_engine(os.environ['MYSQL_CONNECTION_URL'], connect_args={'ssl_ca': certifi.where(), 'ssl_verify_cert': True, 'ssl_verify_identity': True}, pool_recycle=300)
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+        if req.method == 'GET':
+            if req.headers.get('Content-Type') == 'application/json':
+                data = req.get_json()
+                language = data.get('language')
+                order = data['order'] if 'order' in data and data['order'] is not None else 'desc'
+                offset = data.get('offset')
+                limit = data.get('limit')
+
+            else:
+                language = req.params['language'] if 'language' in req.params else None
+                order = req.params['order'] if 'order' in req.params else 'desc'
+                offset = int(req.params['offset']) if 'offset' in req.params else None
+                limit = int(req.params['limit']) if 'limit' in req.params else None
+
+            parsed_url = urlparse(req.url)
+            cache_name = f'{parsed_url.path}?{parsed_url.query}' if len(parsed_url.query) > 0 else parsed_url.path
+            cached_data = get_cache(cache_name)
+
+            if cached_data is None:
+                Session = sessionmaker(bind=engine)
+                session = Session()
+
+                try:
+                    words = []
+                    query = session.query(Word)
+
+                    if language is not None:
+                        query = query.filter(Word.language.like(language))
+
+                    if order == 'asc':
+                        query = query.order_by(Word.timestamp)
+                    elif order == 'desc':
+                        query = query.order_by(desc(Word.timestamp))
+                    else:
+                        return func.HttpResponse(status_code=400, mimetype='', charset='')
+
+                    query = query.limit(100 if limit is None else limit)
+
+                    if offset is not None:
+                        query = query.offset(offset)
+
+                    for word in query.all():
+                        words.append({
+                            'id': word.id,
+                            'name': word.name,
+                            'language': word.language,
+                            'timestamp': int(word.timestamp.replace(tzinfo=timezone.utc).timestamp())
+                        })
+
+                    json_data = json.dumps(words)
+                    set_cache(cache_name, json_data)
+
+                    return func.HttpResponse(json_data, status_code=200, mimetype='application/json', charset='utf-8')
+
+                finally:
+                    session.close()
+
+            else:
+                return func.HttpResponse(cached_data, status_code=200, mimetype='application/json', charset='utf-8')
+
+    except Exception as e:
+        logging.error(f'{e}')
+
+        return func.HttpResponse(json.dumps({
+            'error': {
+                'message': str(e),
+                'type': type(e).__name__}
+        }),
+            status_code=400,
+            mimetype='application/json',
+            charset='utf-8')
